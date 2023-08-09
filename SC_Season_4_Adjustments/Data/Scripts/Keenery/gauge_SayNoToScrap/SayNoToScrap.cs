f6f886dccf9a3eacf6026509fb191d3c069bc7c3
MuzzMuzz <48306411+MuzzMuzz@users.noreply.github.com>
2023-08-08 20:26:14 -0600

    step one: cover yourself in blood

---
+++ SC_Season_4_Adjustments/Data/Scripts/Keenery/gauge_SayNoToScrap/SayNoToScrap.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game;
+using Sandbox.Definitions;
+using VRage.Game.Components;
+using VRage.Utils;
+
+namespace gauge.SayNoToScrap
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+    class SayNoToScrap : MySessionComponentBase
+    {
+        public override void BeforeStart()
+        {
+            foreach (MyDefinitionBase def in MyDefinitionManager.Static.GetAllDefinitions())
+            {
+
+                MyComponentDefinition c = def as MyComponentDefinition;
+
+                if (c == null)
+                {
+                    continue;
+                }
+
+                c.DropProbability = 0;
+            }
+        }
+    }
+}
