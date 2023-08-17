fed4633e6a7aea06e5949a720b287a24d184dc61
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-08-17 01:16:31 -0500

    ok ballpark values for t2-3

--- Starcore-MA-Weapons-Natemod-Dev/Data/Scripts/CoreParts/MA_Laser_T2.cs
+++ Starcore-MA-Weapons-Natemod-Dev/Data/Scripts/CoreParts/MA_Laser_T2.cs
@@ -152,7 +152,7 @@ namespace Scripts {
                     HeatPerShot = 1, // Heat generated per shot.
                     MaxHeat = 3600, // Max heat before weapon enters cooldown (70% of max heat).
                     Cooldown = .1f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
-                    HeatSinkRate = 10, // Amount of heat lost per second.
+                    HeatSinkRate = 60, // Amount of heat lost per second.
                     DegradeRof = false, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 0, // Use this if you don't want the weapon to fire an entire physical magazine before stopping to reload. Should not be more than your magazine capacity.
                     DelayAfterBurst = 0, // How long to spend "reloading" after each burst. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
