f6a6954c9463cbb7fa86f7ad7acbf9369cef1207
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-08-19 19:47:59 -0500

    decaltest

--- Starcore-MA-Weapons-Natemod-Dev/Data/Scripts/CoreParts/MA_Laser_T3_Ammo.cs
+++ Starcore-MA-Weapons-Natemod-Dev/Data/Scripts/CoreParts/MA_Laser_T3_Ammo.cs
@@ -299,6 +299,18 @@ namespace Scripts
                 ModelName = "",
                 VisualProbability = 1f,
                 ShieldHitDraw = false,
+                Decals = new DecalDef
+                {
+                    MaxAge = 3600,
+                    Map = new[]
+                    {
+                        new TextureMapDef
+                        {
+                            HitMaterial = "Metal",
+                            DecalMaterial = "MA_Laser_T4_Decal",
+                        },
+                    },
+                },
                 Particles = new AmmoParticleDef
                 {
                     Ammo = new ParticleDef
