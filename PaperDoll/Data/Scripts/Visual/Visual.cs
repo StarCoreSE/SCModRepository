d432ff27dad5a15080e180cd936899c68384e094
MuzzMuzz <48306411+MuzzMuzz@users.noreply.github.com>
2023-08-23 03:31:22 -0600

    its 3:30am stop rotating transforms in your head

--- PaperDoll/Data/Scripts/Visual/Visual.cs
+++ PaperDoll/Data/Scripts/Visual/Visual.cs
@@ -19,6 +19,12 @@ using VRageRender;
 using VRage.ObjectBuilders;
 using Sandbox.Game.Entities.Cube;
 using VRage.GameServices;
+using System.Linq;
+using Sandbox;
+using BlendTypeEnum = VRageRender.MyBillboard.BlendTypeEnum;
+using VRage.Render.Scene;
+using System.Drawing;
+using Color = VRageMath.Color;
 
 namespace klime.Visual
 {
@@ -31,6 +37,15 @@ namespace klime.Visual
         public double scale, tempscale = 0.8;
         internal Task GridTask = new Task();
         Vector3D relTrans, relForward, relUp;
+        public BoundingBoxD gridBox;
+        public MatrixD gridMatrix;
+        private BoundingBoxD aabb;
+        public Color BillboardRED;
+        public Vector4 color;
+       
+        public bool runonce = false;
+        private MyStringId Material = MyStringId.GetOrCompute("Square");
+        public MyStringHash MaterialHash = MyStringHash.GetOrCompute("SciFi");
 
         public GridR(MyCubeGrid grid, EntRender entRender = null)
         {
@@ -42,12 +57,36 @@ namespace klime.Visual
         {
             renderMatrix.Translation += Vector3D.TransformNormal(relTrans, renderMatrix);
             grid.WorldMatrix = renderMatrix;
+            gridMatrix = renderMatrix;
+            gridBox = grid.PositionComp.LocalAABB;
+
+            // renderMatrix.Translation += renderMatrix.Forward;
+            MatrixD cameramatrixD = MyAPIGateway.Session.Camera.WorldMatrix;
+            Vector3D cameraTranslation = MyAPIGateway.Session.Camera.WorldMatrix.Translation;
+            Vector3D cameraForward = MyAPIGateway.Session.Camera.WorldMatrix.Forward;
+            Vector3D cameraDown = MyAPIGateway.Session.Camera.WorldMatrix.Down;
+            Vector3D cameraRight = MyAPIGateway.Session.Camera.WorldMatrix.Right;
+            Vector3D leftCameraVector = MyAPIGateway.Session.Camera.WorldMatrix.Left;
+            Vector3D upCameraVector = MyAPIGateway.Session.Camera.WorldMatrix.Up;
+            Vector3D backgroundvector =
+                cameraTranslation +
+                (cameraForward * 1f) 
+              + (cameraRight * 0.75f) 
+              + (cameraDown * 0.35f); ;
+
+            Vector4 color = (Color.Lime * 0.05f).ToVector4();
+            MyTransparentGeometry.AddBillboardOriented(Material, color, backgroundvector, leftCameraVector, upCameraVector, 0.25f, 0.25f, null);
+
+            //MyTransparentGeometry.AddLineBillboard(MyStringId.GetOrCompute("WeaponLaser"), Color.White.ToVector4(), backgroundvector + leftCameraVector, cameraRight, 0.5f, 0.5f, BlendTypeEnum.SDR);
+            //MySimpleObjectDraw.DrawAttachedTransparentBox(ref gridMatrix, ref gridBox, ref BillboardRED, uint.MaxValue ,ref cameramatrixD, MySimpleObjectRasterizer.SolidAndWireframe, wiredivratio, 0.04f, MyStringId.GetOrCompute("Square"), MyStringId.GetOrCompute("WeaponLaser"), false, MyBillboard.BlendTypeEnum.SDR);
+
+
         }
 
         public void DoRescale()
         {
             var volume = grid.PositionComp.WorldVolume;
-            scale = 0.029 / volume.Radius * (grid.GridSizeEnum == MyCubeSize.Small ? 0.8 : 1);
+            scale = 0.039 / volume.Radius * (grid.GridSizeEnum == MyCubeSize.Small ? 0.8 : 1);
             relTrans = Vector3D.TransformNormal(grid.WorldMatrix.Translation - grid.PositionComp.WorldAABB.Center, MatrixD.Transpose(grid.WorldMatrix)) * scale;
             grid.PositionComp.Scale = (float)scale;
         }
@@ -55,10 +94,12 @@ namespace klime.Visual
         public void DoCleanup()
         {
             grid.ChangeGridOwnership(MyAPIGateway.Session.Player.IdentityId, MyOwnershipShareModeEnum.Faction);
-            if (grid.IsPowered) grid.SwitchPower();
-            
-            
-            
+
+
+            //if (grid.IsPowered) grid.SwitchPower();
+
+
+
             List<IMySlimBlock> allBlocks = new List<IMySlimBlock>();
 
             IMyCubeGrid iGrid = grid as IMyCubeGrid;
@@ -75,17 +116,26 @@ namespace klime.Visual
             foreach (var block in allBlocks)
             {
                 block.Dithering = 2.7f;
-                block.CubeGrid.Render.ShadowBoxLod = false;
-                block.CubeGrid.Render.EnableColorMaskHsv = true;
-                block.CubeGrid.Render.OffsetInVertexShader = true;
-               block.CubeGrid.Transparent = true;
-               block.CubeGrid.Render.Transparency = 0.01f;
-               block.CubeGrid.FastCastShadowResolve = false;
-              // block.CubeGrid.Render.UpdateTransparency();
-              // block.CubeGrid.Render.UpdateRenderObject(true, false);
-                block.UpdateVisual();
+                //block.CubeGrid
+
+             //   gridMatrix = block.CubeGrid.WorldMatrix;
+
+              //  gridBox = new BoundingBoxD();
+            //    gridBox = block.CubeGrid.PositionComp.LocalAABB;
+
+               // block.GetWorldBoundingBox(out gridBox);
+               // MySimpleObjectDraw.DrawTransparentBox(ref gridMatrix, ref gridBox, ref BillboardRED, MySimpleObjectRasterizer.Solid, 1, 0.04f, MyStringId.GetOrCompute("Square"), MyStringId.GetOrCompute("Square"), false, -1, MyBillboard.BlendTypeEnum.PostPP,1, null);
+           //   block.CubeGrid.Render.ShadowBoxLod = false;
+           //   block.CubeGrid.Render.EnableColorMaskHsv = true;
+           //   block.CubeGrid.Render.OffsetInVertexShader = true;
+           //  block.CubeGrid.Transparent = true;
+           //  block.CubeGrid.Render.Transparency = 0.01f;
+           //  block.CubeGrid.FastCastShadowResolve = false;
+           // // block.CubeGrid.Render.UpdateTransparency();
+           //   block.CubeGrid.Render.UpdateRenderObject(true, false);
+           //   block.UpdateVisual();
                 // block.FatBlock?.Render.UpdateTransparency();
-                // grid.ChangeColorAndSkin(grid.GetCubeBlock(block.Position), orangeHSVOffset);
+                // grid.ChangeColorAndSkin(grid.GetCubeBlock(block.Position), purpleHSVoffset);
             }
 
             foreach (var fatblock in grid.GetFatBlocks())
@@ -102,17 +152,17 @@ namespace klime.Visual
             // grid.Render.Transparency = -0.01f;
 
 
-            // string OrangeHex = "#FF0000";
-            //    Vector3 orangeHSVOffset = MyColorPickerConstants.HSVToHSVOffset(ColorExtensions.ColorToHSV(ColorExtensions.HexToColor(OrangeHex)));
-            //    orangeHSVOffset = new Vector3((float)Math.Round(orangeHSVOffset.X, 2), (float)Math.Round(orangeHSVOffset.Y, 2), (float)Math.Round(orangeHSVOffset.Z, 2));
-            // grid.ChangeColorAndSkin(grid.GetCubeBlock(block.Position), orangeHSVOffset);
+            // string purpleHEx = "#FF0000";
+            //    Vector3 purpleHSVoffset = MyColorPickerConstants.HSVToHSVOffset(ColorExtensions.ColorToHSV(ColorExtensions.HexToColor(purpleHEx)));
+            //    purpleHSVoffset = new Vector3((float)Math.Round(purpleHSVoffset.X, 2), (float)Math.Round(purpleHSVoffset.Y, 2), (float)Math.Round(purpleHSVoffset.Z, 2));
+            // grid.ChangeColorAndSkin(grid.GetCubeBlock(block.Position), purpleHSVoffset);
             // List<IMySlimBlock> allBlocks = new List<IMySlimBlock>();
             //    IMyCubeGrid iGrid = grid as IMyCubeGrid;
             //    iGrid.GetBlocks(allBlocks);
             // 
-            //     //grid.ColorBlocks(grid.Min, grid.Max, orangeHSVOffset, false, false);
-            //     ////iGrid.ColorBlocks(iGrid.Min, iGrid.Max, orangeHSVOffset);
-            //     ////grid.ColorGrid(orangeHSVOffset, false, false);
+            //     //grid.ColorBlocks(grid.Min, grid.Max, purpleHSVoffset, false, false);
+            //     ////iGrid.ColorBlocks(iGrid.Min, iGrid.Max, purpleHSVoffset);
+            //     ////grid.ColorGrid(purpleHSVoffset, false, false);
             // 
 
             //   //grid.Render.Transparency = -0.01f;
@@ -122,10 +172,11 @@ namespace klime.Visual
         {
             if (block != null)
             {
+
                 block.Enabled = false;
                 block.Render.ShadowBoxLod = false;
                 block.SlimBlock.Dithering = 1.95f; // this works!
-                block.Visible = true;
+                block.Visible = false;
                 block.SlimBlock.UpdateVisual();
                 block.Render.UpdateTransparency();
 
@@ -178,7 +229,7 @@ namespace klime.Visual
         public List<Vector3I> FatDelList = new List<Vector3I>();
         public Dictionary<IMyCubeBlock, int> DelDict = new Dictionary<IMyCubeBlock, int>();
         public Dictionary<Vector3I, int> SlimDelDict = new Dictionary<Vector3I, int>();
-
+        public MyStringHash stringHash = MyStringHash.GetOrCompute("Hazard_Armor");
         public Dictionary<Vector3I, float> BlockIntegrityDict = new Dictionary<Vector3I, float>();
         public Dictionary<Vector3I, float> FatBlockIntegrityDict = new Dictionary<Vector3I, float>();
         public List<DamageEntry> DamageEntries = new List<DamageEntry>();
@@ -210,7 +261,7 @@ namespace klime.Visual
                     string redHex = "#FF0000";
                     Vector3 redHSVOffset = MyColorPickerConstants.HSVToHSVOffset(ColorExtensions.ColorToHSV(ColorExtensions.HexToColor(redHex)));
                     redHSVOffset = new Vector3((float)Math.Round(redHSVOffset.X, 2), (float)Math.Round(redHSVOffset.Y, 2), (float)Math.Round(redHSVOffset.Z, 2));
-                    subgrid.grid.ChangeColorAndSkin(subgrid.grid.GetCubeBlock(slim.Position), redHSVOffset);
+                    subgrid.grid.ChangeColorAndSkin(subgrid.grid.GetCubeBlock(slim.Position), redHSVOffset, stringHash);
                     
                  
                     slim.UpdateVisual();
@@ -260,7 +311,7 @@ namespace klime.Visual
                                   "\nFatBlock Damage Last 10 Seconds: " + FormatDamage(fatBlockDamageLast10Seconds) +
                                   "\nSlim Blocks Destroyed: " + SlimBlocksDestroyed +
                                   "\nFat Blocks Destroyed: " + FatBlocksDestroyed;
-            //MyAPIGateway.Utilities.ShowNotification(damageMessage, 16, MyFontEnum.Red); // this is the notification that displays the debug message, use texthudAPI instead
+            MyAPIGateway.Utilities.ShowNotification(damageMessage, 16, MyFontEnum.Red); // this is the notification that displays the debug message, use texthudAPI instead
         }
         public void UpdateMatrix(MatrixD renderMatrix, MatrixD rotMatrix)
         {
@@ -337,6 +388,8 @@ namespace klime.Visual
             renderMatrix = rotMatrix * renderMatrix;
             renderMatrix.Translation = origTranslation;
             foreach (var subgrid in gridGroup) { if (subgrid.grid != null) subgrid.UpdateMatrix(renderMatrix); }
+
+            // I don't know why this works but it does. Don't touch it.
         }
     }
 
@@ -349,6 +402,9 @@ namespace klime.Visual
         public ushort netID = 39302;
         public bool isClosed;
         public double xOffset, yOffset, rotOffset;
+        public List<IMySlimBlock> BlocksForBillboards = new List<IMySlimBlock>();
+        public List<MyBillboard> persistantbillboards = new List<MyBillboard>();
+        public Color BillboardRED;
 
         public EntVis(MyCubeGrid realGrid, double xOffset, double yOffset, double rotOffset)
         {
@@ -368,11 +424,12 @@ namespace klime.Visual
         public void BlockRemoved(Vector3I pos) => visGrid?.DoBlockRemove(pos);
 
         public void GenerateClientGrids()
-        {
+        {   
             HandleException(() =>
             {
                 var realOB = (MyObjectBuilder_CubeGrid)realGrid.GetObjectBuilder();
                 realOB.CreatePhysics = false;
+                
                 MyEntities.RemapObjectBuilder(realOB);
                 MyAPIGateway.Entities.CreateFromObjectBuilderParallel(realOB, false, CompleteCall);
             }, "generating client grids");
@@ -384,11 +441,44 @@ namespace klime.Visual
             {
                 if (isClosed) return;
                 var grid = (MyCubeGrid)obj;
-                grid.SyncFlag = grid.Save = grid.Render.NearFlag = grid.Render.FadeIn = false;
-                grid.Render.CastShadows = false;
-                grid.DisplayName = "";
-                grid.RemoveFromGamePruningStructure();
-                MyAPIGateway.Entities.AddEntity(grid);
+                grid.SyncFlag = grid.Save = grid.Render.NearFlag = grid.Render.FadeIn = grid.Render.FadeOut = grid.Render.CastShadows = grid.Render.NeedsResolveCastShadow = false;
+                grid.GridPresenceTier = MyUpdateTiersGridPresence.Tier1;
+                
+
+                //      grid.Render.PersistentFlags -= MyPersistentEntityFlags2.CastShadows;
+                //      grid.DisplayName = "";
+                //      //grid.IsPreview = true;
+                //      grid.GridPresenceTier = MyUpdateTiersGridPresence.Tier1;
+                //      grid.RemoveFromGamePruningStructure();
+                //       string purpleHEx = "#7851A9";
+                //          Vector3 purpleHSVoffset = MyColorPickerConstants.HSVToHSVOffset(ColorExtensions.ColorToHSV(ColorExtensions.HexToColor(purpleHEx)));
+                //          purpleHSVoffset = new Vector3((float)Math.Round(purpleHSVoffset.X, 2), (float)Math.Round(purpleHSVoffset.Y, 2), (float)Math.Round(purpleHSVoffset.Z, 2));
+                //      grid.ColorGrid(purpleHSVoffset, false);
+                //    grid.GetCubeBlock(BlocksForBillboards);
+
+                // var bruh = new Vector3I(0, 0, 0);
+                //    var igrid = (IMyCubeGrid)grid;
+
+                // igrid.GetBlocks(BlocksForBillboards);
+                //    BuildBlocksClient
+                //  grid.ChangeColorAndSkin(grid.BuildBlocksClient)
+                //     var igrid = (IMyCubeGrid)grid;
+
+                //     igrid.GetBlocks(BlocksForBillboards);
+
+          //       foreach (var block in BlocksForBillboards)
+          //       {
+          //         var squaremat =  MyStringId.GetOrCompute("Square");
+          //           var color = Color.Red;
+          //           BoundingBoxD box;
+          //           box = new BoundingBoxD(block.Min, block.Max);
+          //           MatrixD MatrixD = block.CubeGrid.WorldMatrix;
+          //           BillboardRED = Color.Red;
+          //        uint renderObjectID = block.CubeGrid.Render.GetRenderObjectID();
+          //           MySimpleObjectDraw.DrawTransparentBox(ref MatrixD, ref box, ref color, MySimpleObjectRasterizer.Solid, 1, 0.04f, squaremat, squaremat, false, -1 , BlendTypeEnum.AdditiveTop, 1, persistantbillboards);
+          //           //MySimpleObjectDraw.DrawAttachedTransparentBox(ref realGridBaseMatrix,ref box,ref BillboardRED, renderObjectID,ref MatrixD,MySimpleObjectRasterizer.SolidAndWireframe, 4,0.001f,squaremat,squaremat,false);
+          //    }
+                MyAPIGateway.Entities.AddEntity(grid); //right hewre
                 visGrid = new GridG(new GridR(grid), rotOffset);
             }, "completing the call");
         }
@@ -599,7 +689,7 @@ namespace klime.Visual
                     MyCubeGrid cGrid = ent as MyCubeGrid;
                     if (cGrid != null && cGrid.Physics != null)
                     {
-                        allVis.Add(new EntVis(cGrid, 0.12, 0.03, 0));
+                        allVis.Add(new EntVis(cGrid, 0.10, 0.05, 0));
                         viewState = ViewState.Locked;
                     }
                     else viewState = ViewState.GoToIdleWC;
@@ -616,10 +706,10 @@ namespace klime.Visual
         }
         public override void Draw()
         {
-            if (MyAPIGateway.Utilities.IsDedicated)
-            {
-                return;
-            }
+
+
+
+
             if (MyAPIGateway.Utilities.IsDedicated) return;
             IMyCharacter charac = MyAPIGateway.Session.Player?.Character;
             if (charac == null) return;
@@ -662,7 +752,7 @@ namespace klime.Visual
                                     entVis.Close();
                                 }
                                 allVis.Clear();
-                                EntVis vis = new EntVis(cGrid, 0.12, 0.03, 0);
+                                EntVis vis = new EntVis(cGrid, 0.10, 0.05, 0);
                                 allVis.Add(vis);
                             }
                         }
