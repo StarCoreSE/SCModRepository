b8117cbb4762af7c385cd24908a6fd550651d7f4
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2023-08-22 22:33:16 -0400

    New Files
    Created new Terminal and Toolbar Actions
    Applied Modifier to Whole Grid
    various other bits and bobs, currently updating when something occurs is having issues

---
+++ SCDefenseBlocks/Data/Scripts/dynresist/DynamicResistence_Core.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Sandbox.Game;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using Sandbox.Definitions;
+using Sandbox.Common.ObjectBuilders;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace DynamicResistence
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_ConveyorSorter), false, "LargeBlockConveyorSorter")]
+    public class DynamicResistLogic : MyGameLogicComponent
+    {
+        private IMyConveyorSorter dynResistBlock;
+
+        internal static DynamicResistLogic Instance;
+        private bool ModifiedTerminalControls = false;
+
+        public float HullPolarization { get; set; }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            if (!MyAPIGateway.Session.IsServer) return; // Only do calculations serverside
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            DynaResistTerminalControls.DoOnce(ModContext);
+            dynResistBlock = (IMyConveyorSorter)Entity;
+            if (dynResistBlock == null || dynResistBlock.CubeGrid.Physics == null) return;
+            dynResistBlock.PropertiesChanged += DynResistValueChanged;
+
+            Instance = this;
+
+            SetupTerminalControls();
+        }
+
+        private void DynResistValueChanged(IMyTerminalBlock obj)
+        {
+            if (obj.EntityId != dynResistBlock.EntityId) return;
+
+            var allBlocks = new List<IMySlimBlock>();
+            dynResistBlock.CubeGrid.GetBlocks(allBlocks);
+
+            float finalResistanceModifier = 0f;
+
+            if (dynResistBlock.Enabled)
+            {
+                foreach (var block in allBlocks)
+                {
+                    var dynamicResistLogic = block.FatBlock?.GameLogic?.GetAs<DynamicResistLogic>();
+
+                    if (dynamicResistLogic != null)
+                    {
+                        float hullPolarization = dynamicResistLogic.HullPolarization;
+
+                        float minHullPolarization = 0;
+                        float maxHullPolarization = 30;
+                        float minResistanceModifier = 1.0f;
+                        float maxResistanceModifier = 0.7f;
+
+                        float t = (hullPolarization - minHullPolarization) / (float)(maxHullPolarization - minHullPolarization);
+                        float resistanceModifier = minResistanceModifier + t * (maxResistanceModifier - minResistanceModifier);
+
+                        // Round the resistance modifier to two decimal places
+                        resistanceModifier = (float)Math.Round(resistanceModifier, 2);
+
+                        block.BlockGeneralDamageModifier = resistanceModifier;
+
+                        finalResistanceModifier = resistanceModifier;
+                    }
+                }
+            }
+            else
+            {
+                MyAPIGateway.Utilities.ShowNotification("Block is disabled", 10000, MyFontEnum.Red);
+
+            }
+            
+
+            MyAPIGateway.Utilities.ShowNotification("Current Resistence Value:" + finalResistanceModifier, 5000, MyFontEnum.Green);
+
+            /*if (dynResistBlock.Enabled)
+            {
+                MyAPIGateway.Utilities.ShowNotification("Block is enabled", 10000, MyFontEnum.Green);
+            }
+            else
+            {
+                MyAPIGateway.Utilities.ShowNotification("Block is disabled", 10000, MyFontEnum.Red);
+            }*/
+        }
+
+        public static void SetupTerminalControls()
+        {
+            if (Instance.ModifiedTerminalControls)
+                return;
+
+            Instance.ModifiedTerminalControls = true;
+
+            /*SetupActions();*/
+            SetupControls();
+        }
+
+        private static void SetupControls()
+        {
+            List<IMyTerminalControl> controls;
+            MyAPIGateway.TerminalControls.GetControls<IMyConveyorSorter>(out controls);
+
+            foreach (var c in controls)
+            {
+                switch (c.Id)
+                {
+                    case "DrainAll":
+                    case "blacklistWhitelist":
+                    case "CurrentList":
+                    case "removeFromSelectionButton":
+                    case "candidatesList":
+                    case "addToSelectionButton":
+                        c.Visible = CombineFunc.Create(c.Visible, Visible);
+                        break;
+                }
+            }
+        }
+
+        private static bool Visible(IMyTerminalBlock block)
+        {
+            return block != null && !(block.GameLogic is DynamicResistLogic);
+        }
+
+        public override void Close()
+        {
+            if (dynResistBlock != null)
+            {
+                dynResistBlock.PropertiesChanged -= DynResistValueChanged;
+            }
+        }
+    }
+}
---
+++ SCDefenseBlocks/Data/Scripts/dynresist/DynamicResistence_TerminalControls.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Sandbox.Game.Localization;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.Utils;
+using VRageMath;
+
+namespace DynamicResistence
+{
+    public static class DynaResistTerminalControls
+    {
+        const string IdPrefix = "SCDefMod_"; // highly recommended to tag your properties/actions like this to avoid colliding with other mods'
+
+        static bool Done = false;
+
+        public static void DoOnce(IMyModContext context) 
+        {
+            if(Done)
+                return;
+            Done = true;
+
+            CreateControls();
+            CreateActions(context);
+        }
+
+        static bool CustomVisibleCondition(IMyTerminalBlock b)
+        {
+            return b?.GameLogic?.GetAs<DynamicResistLogic>() != null;
+        }
+
+        static void CreateControls()
+        {
+            {
+                IMyTerminalControlSlider polarizationValueSlider = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlSlider, IMyConveyorSorter>(IdPrefix + "HullPolarization");
+                polarizationValueSlider.Title = MyStringId.GetOrCompute("Hull Polarization");
+
+                polarizationValueSlider.Tooltip = MyStringId.GetOrCompute("Adjusts the mount of Damage Absorbed by the Block");
+                polarizationValueSlider.SetLimits(0, 30);
+                polarizationValueSlider.Writer = (b, sb) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        float v = logic.HullPolarization;
+                        sb.Append(Math.Round(v, 0, MidpointRounding.ToEven)).Append("%");
+                    }
+                };
+                polarizationValueSlider.Visible = p => p.BlockDefinition.SubtypeId.Contains("LargeBlockConveyorSorter");
+                polarizationValueSlider.Enabled = p => p.BlockDefinition.SubtypeId.Contains("LargeBlockConveyorSorter");
+                polarizationValueSlider.Getter = (b) => b?.GameLogic?.GetAs<DynamicResistLogic>()?.HullPolarization ?? 0;
+                polarizationValueSlider.Setter = (b, v) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                        logic.HullPolarization = MathHelper.Clamp(v, 0f, 30f);
+                };
+                polarizationValueSlider.SupportsMultipleBlocks = true;
+                MyAPIGateway.TerminalControls.AddControl<IMyConveyorSorter>(polarizationValueSlider);
+            }
+        }
+
+        static void CreateActions(IMyModContext context)
+        {
+            {
+                var increasePolarization = MyAPIGateway.TerminalControls.CreateAction<IMyConveyorSorter>(IdPrefix + "PolarizationIncrease");
+
+                increasePolarization.Name = new StringBuilder("Increase Polarization");
+                increasePolarization.ValidForGroups = true;
+                increasePolarization.Icon = @"Textures\GUI\Icons\Actions\CharacterToggle.dds";
+                increasePolarization.Action = (b) => 
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        logic.HullPolarization = logic.HullPolarization + 1;
+                    }
+                };
+                increasePolarization.Writer = (b, sb) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        float v = logic.HullPolarization;
+                        sb.Append(Math.Round(v, 0, MidpointRounding.ToEven)).Append("%");
+                    }
+                };
+                increasePolarization.InvalidToolbarTypes = new List<MyToolbarType>()
+                {
+                    MyToolbarType.ButtonPanel,
+                    MyToolbarType.Character,
+                };
+                increasePolarization.Enabled = CustomVisibleCondition;
+
+                MyAPIGateway.TerminalControls.AddAction<IMyConveyorSorter>(increasePolarization);
+            }
+
+            //Decrease Polarization
+            {
+                var decreasePolarization = MyAPIGateway.TerminalControls.CreateAction<IMyConveyorSorter>(IdPrefix + "PolarizationDecrease");
+
+                decreasePolarization.Name = new StringBuilder("Decrease Polarization");
+                decreasePolarization.ValidForGroups = true;
+                decreasePolarization.Icon = @"Textures\GUI\Icons\Actions\CharacterToggle.dds";
+                decreasePolarization.Action = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        logic.HullPolarization = logic.HullPolarization - 1;
+                    }
+                };
+                decreasePolarization.Writer = (b, sb) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        float v = logic.HullPolarization;
+                        sb.Append(Math.Round(v, 0, MidpointRounding.ToEven)).Append("%");
+                    }
+                };
+                decreasePolarization.InvalidToolbarTypes = new List<MyToolbarType>( )
+                {
+                    MyToolbarType.ButtonPanel,
+                    MyToolbarType.Character,
+                };
+                decreasePolarization.Enabled = CustomVisibleCondition;
+
+                MyAPIGateway.TerminalControls.AddAction<IMyConveyorSorter>(decreasePolarization);
+            }
+        }
+
+        /*static void CreateProperties()
+        {
+            // Terminal controls automatically generate properties like these, but you can also add new ones manually without the GUI counterpart.
+            // The main use case is for PB to be able to read them.
+            // The type given is only limited by access, can only do SE or .NET types, nothing custom (except methods because the wrapper Func/Action is .NET).
+            // For APIs, one can send a IReadOnlyDictionary<string, Delegate> for a list of callbacks. Just be sure to use a ImmutableDictionary to avoid getting your API hijacked.
+            {
+                var CurrentPolarizationValue = MyAPIGateway.TerminalControls.CreateProperty<Vector3, IMyConveyorSorter>(IdPrefix + "CurrentPolarValue");
+
+                CurrentPolarizationValue.Getter = (b) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        logic.HullPolarization = logic.HullPolarization;
+                    }
+                };
+
+                CurrentPolarizationValue.Setter = (b, v) =>
+                {
+                    var logic = b?.GameLogic?.GetAs<DynamicResistLogic>();
+                    if (logic != null)
+                    {
+                        logic.HullPolarization = logic.HullPolarization;
+                    }
+                };
+
+                MyAPIGateway.TerminalControls.AddControl<IMyConveyorSorter>(CurrentPolarizationValue);
+
+
+                // a mod or PB can use it like:
+                //Vector3 vec = gyro.GetValue<Vector3>("YourMod_SampleProp");
+                // just careful with sending mutable reference types, there's no serialization inbetween so the mod/PB can mutate your reference.
+            }
+          }*/
+
+    }
+}
