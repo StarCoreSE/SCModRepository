8feecc096691f82baba6bc1d2d0bc1ea28f53c35
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-09-06 02:28:26 -0500

    Revert "Merge pull request #78 from InvalidArgument3/blockerplateedits"
    This reverts commit 4a00464c99c4728b710eb855f7ff478eb4440994, reversing
    changes made to 09bc3895cd436b900ec0987238e7fa4dd13d99a1.

--- StarCoreLinkUtility/Data/Scripts/AdminBugReporterUtility/script.cs
+++ StarCoreLinkUtility/Data/Scripts/AdminBugReporterUtility/script.cs
@@ -23,33 +23,26 @@ namespace invalid.BugReporter
                 return;
             }
 
-            //if (MyAPIGateway.Input.IsKeyPress(MyKeys.LeftAlt) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.F2) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
-            //{
-            //   
-            //    MyVisualScriptLogicProvider.OpenSteamOverlay("https://steamcommunity.com/linkfilter/?url=https://github.com/StarCoreSE/SCModRepository/issues/new/choose");
-			//			
-            //}
+            if (MyAPIGateway.Input.IsKeyPress(MyKeys.LeftShift) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.F2) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
+            {
+               
+                MyVisualScriptLogicProvider.OpenSteamOverlay("https://steamcommunity.com/linkfilter/?url=https://github.com/StarCoreSE/SCModRepository/issues/new/choose");
+						
+            }
 			
-            if (MyAPIGateway.Input.IsKeyPress(MyKeys.RightAlt) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.P) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
+            if (MyAPIGateway.Input.IsKeyPress(MyKeys.LeftAlt) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.P) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
             {
                
                 MyVisualScriptLogicProvider.OpenSteamOverlay("https://steamcommunity.com/linkfilter/?url=https://docs.google.com/spreadsheets/d/1RmhKu3lAIhvy6xMpsmaNAmTV8Ly44NOIQJgQm5OJtlo");
 						
-            }	
-
-            if (MyAPIGateway.Input.IsKeyPress(MyKeys.RightAlt) && MyAPIGateway.Input.IsNewKeyPressed(MyKeys.O) && ValidInput()) //hey dumbass, use this before the url. fucking keen https://steamcommunity.com/linkfilter/?url={url}
-            {
-               
-                MyVisualScriptLogicProvider.OpenSteamOverlay("https://steamcommunity.com/linkfilter/?url=https://docs.google.com/spreadsheets/d/1RmhKu3lAIhvy6xMpsmaNAmTV8Ly44NOIQJgQm5OJtlo");
-						
-            }				
+            }			
 				
         }
 
 		public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
 		{
 
-            MyAPIGateway.Utilities.ShowMessage("GODFORSAKEN ASTRONAUT", "RightAlt + P to bring up the points menu. RightAlt + O to bring up the wiki." );
+            MyAPIGateway.Utilities.ShowMessage("GODFORSAKEN ASTRONAUT", "Press Shift + F2 to report a bug. You get a gold star for uploading your log. Also Alt + P to bring up the points menu." );
 			
            // MyAPIGateway.Utilities.ShowNotification("\n\n ALT+P = Starcore Pointsheet \n SHIFT+F2 = Report Bug", 30000, "Red"); //this is fucking stupid.
 			
