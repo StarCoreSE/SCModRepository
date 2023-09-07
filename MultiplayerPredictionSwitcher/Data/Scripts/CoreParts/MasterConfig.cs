6e166dbc12fe5211ec131b28aec87f9166234908
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-09-07 17:53:38 -0500

    almost

---
+++ MultiplayerPredictionSwitcher/Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(
+            MA_Afterburner_Large_small,
+            MA_Afterburner_Large_small_smallgrid,
+            MA_Afterburner_Large,
+            MA_Afterburner_Large_5x,
+            Afterburner_LG_Ion_Large,
+			SRB,
+			DETPAK,
+            DETPAK_1x1,
+            DETPAK_3x1
+
+
+            );
+
+        }
+    }
+}
