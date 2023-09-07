39892b906d934e8e7367251517938bfeb4d50053
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-09-07 02:36:25 -0500

    update ma weapon desc

--- Starcore-MA-Weapons-Natemod-Dev/Data/Scripts/CoreParts/MA_Laser_T1_Ammo.cs
+++ Starcore-MA-Weapons-Natemod-Dev/Data/Scripts/CoreParts/MA_Laser_T1_Ammo.cs
@@ -36,7 +36,7 @@ namespace Scripts
             AmmoRound = "MA_Laser_1", // Name of ammo in terminal, should be different for each ammo type used by the same weapon.
             HybridRound = false, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 1f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
-            BaseDamage = 1f, // Direct damage; one steel plate is worth 100.
+            BaseDamage = 50f, // Direct damage; one steel plate is worth 100.
             Mass = 0f, // In kilograms; how much force the impact will apply    to the target.
             Health = 0, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
             BackKickForce = 0f, // Recoil.
