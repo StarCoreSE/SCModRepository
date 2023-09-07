079af40f4b1e9cefe88629e2d1b05b63844198d7
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-09-07 15:24:21 -0500

    client prediction test on srb

--- MA_Afterburner/Data/Scripts/SRBanticringe/SRBanticringeexplodealternate.cs
+++ MA_Afterburner/Data/Scripts/SRBanticringe/SRBanticringeexplodealternate.cs
@@ -5,66 +5,75 @@ using Sandbox.Game.Entities.Blocks;
 using Sandbox.ModAPI;
 using System;
 using VRage.Game.Components;
+using VRage.Game.ModAPI;
 using VRage.ModAPI;
 using VRage.ObjectBuilders;
 using VRageMath;
 
 namespace SRBanticringe
 {
-	[MyEntityComponentDescriptor(typeof(MyObjectBuilder_ConveyorSorter), false, "SC_SRB")]
-	public class SRBexplodealternate : MyGameLogicComponent
-	{
-		private IMyConveyorSorter SRB;
-		private int triggerTick = 0;
-		private const double OFFSET_DISTANCE = 3;
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_ConveyorSorter), false, "SC_SRB")]
+    public class SRBexplodealternate : MyGameLogicComponent
+    {
+        private IMyConveyorSorter SRB;
+        private int triggerTick = 0;
+        private const double OFFSET_DISTANCE = 3;
 
-		public override void Init(MyObjectBuilder_EntityBase objectBuilder)
-		{
-			if (!MyAPIGateway.Session.IsServer) return; // Only do explosions serverside
-			SRB = Entity as IMyConveyorSorter;
-			NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
-		}
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            if (!MyAPIGateway.Session.IsServer) return;
+            SRB = Entity as IMyConveyorSorter;
+            (SRB.CubeGrid as MyCubeGrid).ForceDisablePrediction = true;  // Corrected Line
 
-		public override void UpdateOnceBeforeFrame()
-		{
-			if (SRB == null || SRB.CubeGrid.Physics == null) return;
-			SRB.EnabledChanged += SRBEnabledChanged;
-			NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
-		}
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+            MyVisualScriptLogicProvider.ShowNotification("client prediction disabled???", 10000, "Red");
+        }
 
-		public override void UpdateAfterSimulation()
-		{
-			if (triggerTick == 1)
-			{
-				DoExplosion();
-			}
+        public override void UpdateOnceBeforeFrame()
+        {
+            if (SRB == null || SRB.CubeGrid.Physics == null) return;
+            SRB.EnabledChanged += SRBEnabledChanged;
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
+            
+        }
 
-			triggerTick = 0;
-		}
+        public override void UpdateAfterSimulation()
+        {
+            if (triggerTick == 1)
+            {
+                DoExplosion();
+            }
 
-		private void DoExplosion()
-		{
-			if (SRB == null || SRB.CubeGrid.Physics == null || SRB.Enabled) return;
-			double radius = 30;
-			BoundingSphereD sphere = new BoundingSphereD(SRB.WorldMatrix.Translation + (SRB.WorldMatrix.Forward * OFFSET_DISTANCE), radius); // Apply offset, 10);
-			MyExplosionInfo explosion = new MyExplosionInfo(0f, 10000f, sphere, MyExplosionTypeEnum.CUSTOM, true);
+            triggerTick = 0;
+        }
 
-			MyExplosions.AddExplosion(ref explosion);
-		}
+        private void DoExplosion()
+        {
+            if (SRB == null || SRB.CubeGrid.Physics == null || SRB.Enabled) return;
+            double radius = 30;
+           // BoundingSphereD sphere = new BoundingSphereD(SRB.WorldMatrix.Translation + (SRB.WorldMatrix.Forward * OFFSET_DISTANCE), radius);
+            //MyExplosionInfo explosion = new MyExplosionInfo(0f, 10000f, sphere, MyExplosionTypeEnum.CUSTOM, true);
 
-		private void SRBEnabledChanged(IMyTerminalBlock obj)
-		{
-			if (obj.EntityId != SRB.EntityId) return;
-			triggerTick += 1;
-		}
+            //MyExplosions.AddExplosion(ref explosion);
+        }
+
+        private void SRBEnabledChanged(IMyTerminalBlock obj)
+        {
+            if (obj.EntityId != SRB.EntityId) return;
+            triggerTick += 1;
+
+            // Check if the grid is client-predicted and show a notification
+            bool isPredicted = (SRB.CubeGrid as MyCubeGrid).IsClientPredicted;
+            MyVisualScriptLogicProvider.ShowNotification($"Grid is client-predicted: {isPredicted}", 2000, "Red");
+        }
 
         public override void Close()
         {
             if (SRB != null)
             {
                 SRB.EnabledChanged -= SRBEnabledChanged;
+                (SRB.CubeGrid as MyCubeGrid).ForceDisablePrediction = false;  // Corrected Line
             }
         }
-
     }
 }
