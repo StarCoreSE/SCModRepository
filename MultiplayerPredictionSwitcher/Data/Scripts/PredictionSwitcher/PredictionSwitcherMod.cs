626c0291e8d47f3b431001928f838b4e36791ba8
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-09-07 18:52:58 -0500

    this may be it

---
+++ MultiplayerPredictionSwitcher/Data/Scripts/PredictionSwitcher/PredictionSwitcherMod.cs
@@ -0,0 +1,55 @@
+﻿using Sandbox.Game.Entities;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.Game;
+
+[MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+public class SessionComp : MySessionComponentBase
+{
+    private bool isPredictionDisabled = false;
+
+    public override void LoadData()
+    {
+        if (!MyAPIGateway.Utilities.IsDedicated)
+        {
+            MyAPIGateway.Utilities.MessageEntered += OnMessageEntered;
+        }
+    }
+
+    private void OnMessageEntered(string messageText, ref bool sendToOthers)
+    {
+        if (!MyAPIGateway.Utilities.IsDedicated && messageText.Equals("/toggleprediction"))
+        {
+            isPredictionDisabled = !isPredictionDisabled;
+            MyCubeGrid controlled = MySession.Static?.ControlledGrid;
+            if (controlled != null)
+            {
+                controlled.ForceDisablePrediction = isPredictionDisabled;
+            }
+            MyAPIGateway.Utilities.ShowNotification($"Prediction disabled: {isPredictionDisabled}", 2000, MyFontEnum.Red);
+            sendToOthers = false;
+        }
+    }
+
+    public override void UpdateAfterSimulation()
+    {
+        if (!MyAPIGateway.Utilities.IsDedicated && isPredictionDisabled)
+        {
+            MyCubeGrid controlled = MySession.Static?.ControlledGrid;
+            if (controlled != null)
+            {
+                controlled.ForceDisablePrediction = true;
+            }
+        }
+    }
+
+    protected override void UnloadData()
+    {
+        if (!MyAPIGateway.Utilities.IsDedicated)
+        {
+            MyAPIGateway.Utilities.MessageEntered -= OnMessageEntered;
+        }
+    }
+
+}
