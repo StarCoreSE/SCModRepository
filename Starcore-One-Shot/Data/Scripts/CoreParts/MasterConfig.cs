33eea7ca3b3a7a43dfb25a130539e6b65168b59d
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-09-11 01:06:31 -0500

    move over flares to reecore

--- Starcore-One-Shot/Data/Scripts/CoreParts/MasterConfig.cs
+++ Starcore-One-Shot/Data/Scripts/CoreParts/MasterConfig.cs
@@ -10,7 +10,7 @@ namespace Scripts
             // Enable your definitions using the follow syntax:
             // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
             // PartDefinitions includes both weapons and phantoms
-            PartDefinitions(K_SA_Launcher_ATM_I, K_SA_Launcher_Hunt, SC_Flare);
+            PartDefinitions(K_SA_Launcher_ATM_I, K_SA_Launcher_Hunt);
         }
     }
 }
