0e23e13f7964c4302e624669e84b09c06d33fea3
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-12-08 23:23:53 -0600

    that was the symptom, it was getAIfocus. handle exceptions
    This reverts commit 98564ef3777c8f5c4cb096c485176e08879cf347.

--- PaperDoll/Data/Scripts/Visual/Visual.cs
+++ PaperDoll/Data/Scripts/Visual/Visual.cs
@@ -270,7 +270,7 @@ namespace klime.Visual
 
         private void DisableBlock(IMyFunctionalBlock block)
         {
-            if (block != null)
+            if (block != null && block.Enabled != true)
             {
                 block.Enabled = false;
             }
