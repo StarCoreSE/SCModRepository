dba8ebcbe3d9490053de6077b2f15f330308301f
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-09 23:32:04 -0600

    firing sound for nimrod/resh

--- OnyxArmamentCo/Data/Scripts/CoreParts/NimrodRailgun.cs
+++ OnyxArmamentCo/Data/Scripts/CoreParts/NimrodRailgun.cs
@@ -140,7 +140,7 @@ namespace Scripts {
                 Audio = new HardPointAudioDef
                 {
                     PreFiringSound = "", // Audio for warmup effect.
-                    FiringSound = "ArcWepShipOnyxRailgun_Fire", // Audio for firing.
+                    FiringSound = "Nimrod_Fire", // Audio for firing.
                     FiringSoundPerShot = true, // Whether to replay the sound for each shot, or just loop over the entire track while firing.
                     ReloadSound = "", // Sound SubtypeID, for when your Weapon is in a reloading state
                     NoAmmoSound = "",
