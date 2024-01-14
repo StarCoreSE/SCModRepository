754dd066d40eda7b45cf4353f5885efc41da309d
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-14 03:58:47 -0600

    less component agony

--- Heavy-Assault-Systems/Data/Scripts/CoreParts/Esper_Ammo.cs
+++ Heavy-Assault-Systems/Data/Scripts/CoreParts/Esper_Ammo.cs
@@ -556,10 +556,10 @@ namespace Scripts
                 },
                 EndOfLife = new EndOfLifeDef
                 {
-                    Enable = false,
+                    Enable = true,
                     Radius = 3f, // Radius of AOE effect, in meters.
-                    Damage = 15000f,
-                    Depth = 3f, // Max depth of AOE effect, in meters. 0=disabled, and AOE effect will reach to a depth of the radius value
+                    Damage = 5000f,
+                    Depth = 1f, // Max depth of AOE effect, in meters. 0=disabled, and AOE effect will reach to a depth of the radius value
                     MaxAbsorb = 0f, // Soft cutoff for damage, except for pooled falloff.  If pooled falloff, limits max damage per block.
                     Falloff = Pooled, //.NoFalloff applies the same damage to all blocks in radius
                     //.Linear drops evenly by distance from center out to max radius
