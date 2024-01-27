b71ee104db2f3241d1a2db027ac726c77a4a1303
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-26 22:17:52 -0600

    Update SI_Utility.cs

--- SCDefenseBlocks/Data/Scripts/OneFuckingFolderDeeper/StructuralIntegrity/SI_Utility.cs
+++ SCDefenseBlocks/Data/Scripts/OneFuckingFolderDeeper/StructuralIntegrity/SI_Utility.cs
@@ -9,9 +9,9 @@ namespace StarCore.StructuralIntegrity
    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
    public class SI_Utility : MySessionComponentBase
    {
-      // public static SI_Utility Instance;
-      // 
-      // public bool ControlsCreated = false;
+       public static SI_Utility Instance;
+       
+       public bool ControlsCreated = false;
       // public Networking Networking = new Networking(58936);
       // public List<MyEntity> Entities = new List<MyEntity>();
       // public PacketBlockSettings CachedPacketSettings;
@@ -19,21 +19,21 @@ namespace StarCore.StructuralIntegrity
       // public readonly MyStringId MATERIAL_SQUARE = MyStringId.GetOrCompute("Square");
       // public readonly MyStringId MATERIAL_DOT = MyStringId.GetOrCompute("WhiteDot");
       //
-      // public override void LoadData()
-      // {
-      //     Instance = this;
-      //
-      //     Networking.Register();
-      //
-      //     CachedPacketSettings = new PacketBlockSettings();
-      // }
-      //
-      // protected override void UnloadData()
-      // {
-      //     Instance = null;
-      //
-      //     Networking?.Unregister();
-      //     Networking = null;
-      // }
+       public override void LoadData()
+       {
+           Instance = this;
+      
+          // Networking.Register();
+      
+          // CachedPacketSettings = new PacketBlockSettings();
+       }
+      
+       protected override void UnloadData()
+       {
+           Instance = null;
+      
+           //Networking?.Unregister();
+           //Networking = null;
+       }
    }
 }
