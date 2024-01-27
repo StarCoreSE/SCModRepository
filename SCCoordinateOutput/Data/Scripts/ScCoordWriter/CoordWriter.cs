7d34eb0c716839a40ff5c1e96dfd4f6c766210aa
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-01-26 20:08:08 -0600

    add

---
+++ SCCoordinateOutput/Data/Scripts/ScCoordWriter/CoordWriter.cs
@@ -0,0 +1,81 @@
+﻿using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.ModAPI;
+using VRageMath;
+
+namespace YourName.ModName.Data.Scripts.ScCoordWriter
+{
+    internal class CoordWriter
+    {
+        /*Per-ship:
+         * File name is $"{DateTime}|{ShipName}.json"
+         * Format:
+         *   CSV table:
+         *     StartTime,FactionName,ShipName,OwnerName
+         *     GridSizeValue,Size.X,Size.Y,Size.Z
+         *     Tick,IsAlive,CurrentHealth%,Position.X,Position.Y,Position.Z,Rotation.X,Rotation.Y,Rotation.Z,Rotation.W
+         *     (continued above single row)
+         */
+
+
+        IMyCubeGrid grid;
+        TextWriter writer;
+        Vector3 oldPos;
+        Quaternion oldRot;
+
+        public CoordWriter(IMyCubeGrid grid, string fileExtension = ".scc")
+        {
+            this.grid = grid;
+            writer = MyAPIGateway.Utilities.WriteFileInWorldStorage($"{DateTime.Now:dd-mm-yyyy HHmm} , {grid.EntityId}{fileExtension}", typeof(CoordWriter));
+        }
+
+        public void WriteStartingData(string factionName)
+        {
+            string owner = "";
+
+            if (grid.BigOwners.Count > 0)
+            {
+                List<IMyIdentity> identities = new List<IMyIdentity>();
+                MyAPIGateway.Players.GetAllIdentites(identities);
+                foreach (IMyIdentity ident in identities)
+                {
+                    if (ident.IdentityId == grid.BigOwners[0])
+                    {
+                        owner = ident.DisplayName;
+                        break;
+                    }
+                }
+            }
+
+            writer.WriteLine($"{DateTime.Now},{factionName},{grid.CustomName},{owner}");
+            Vector3I size = Vector3I.Abs(grid.Min) + Vector3I.Abs(grid.Max);
+            writer.WriteLine($"{grid.GridSize},{size.X},{size.Y},{size.Z}");
+            writer.Flush();
+        }
+
+        public void WriteNextTick(int currentTick, bool isAlive, float healthPercent)
+        {
+            Vector3 position = grid.GetPosition();
+            Quaternion rotation = Quaternion.CreateFromForwardUp(grid.WorldMatrix.Forward, grid.WorldMatrix.Up);
+
+            if (position == oldPos && rotation == oldRot)
+                return;
+
+            oldPos = position;
+            oldRot = rotation;
+
+            writer.WriteLine($"{currentTick},{isAlive},{Math.Round(healthPercent, 2)},{position.X},{position.Y},{position.Z},{rotation.X},{rotation.Y},{rotation.Z},{rotation.W}");
+            writer.Flush();
+        }
+
+        public void Close()
+        {
+            writer.Close();
+        }
+    }
+}
---
+++ SCCoordinateOutput/Data/Scripts/ScCoordWriter/sccoords.cs
@@ -0,0 +1,99 @@
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Blocks;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Ingame;
+using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRageMath;
+
+namespace YourName.ModName.Data.Scripts.ScCoordWriter
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Cockpit), false)]
+    public class coordoutput : MyGameLogicComponent
+    {
+        private Sandbox.ModAPI.IMyCockpit Cockpit;
+        private const string fileExtension = ".scc";
+        private int tickCounter = 0;
+        CoordWriter writer;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            if (!MyAPIGateway.Session.IsServer) return; // Only do stuff serverside
+            Cockpit = Entity as Sandbox.ModAPI.IMyCockpit;
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            if (Cockpit == null || Cockpit.CubeGrid.Physics == null) return;
+
+            writer = new CoordWriter(Cockpit.CubeGrid, fileExtension);
+            writer.WriteStartingData("RED");
+
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            if (!MyAPIGateway.Multiplayer.IsServer)
+                return; // Only execute on the server
+
+            if (Cockpit == null || Cockpit.CubeGrid.Physics == null) return;
+            if (Cockpit.IsFunctional) // Only execute when cockpit is functional
+            {
+                if (Cockpit.IsUnderControl) // Only execute when cockpit is manned
+                {
+                    tickCounter++;
+                    if (tickCounter % 30 == 0) // Output once per quarter second
+                    {
+                        writer.WriteNextTick(MyAPIGateway.Session.GameplayFrameCounter, true, 1.0f);
+
+                        // Get current position of cockpit
+
+                        //Vector3D currentPosition = Cockpit.GetPosition();
+                        //// Get grid name
+                        //string gridName = Cockpit.CubeGrid.CustomName;
+                        //
+                        //// Create debug message with current position, tick counter, and grid name
+                        //string message = string.Format("Tick {0}: {1} position: X={2}, Y={3}, Z={4}", tickCounter, gridName, currentPosition.X, currentPosition.Y, currentPosition.Z);
+                        //// Show the debug message
+                        ////MyVisualScriptLogicProvider.ShowNotificationLocal(message, 1000, "Debug");
+                        //
+                        //// Create unique file name based on grid name
+                        //string fileName = gridName + fileExtension;
+                        //
+                        //// Read existing contents of the file, or create an empty string if the file doesn't exist
+                        //string existingContents = "";
+                        //if (MyAPIGateway.Utilities.FileExistsInWorldStorage(fileName, typeof(coordoutput)))
+                        //{
+                        //    var reader = MyAPIGateway.Utilities.ReadFileInWorldStorage(fileName, typeof(coordoutput));
+                        //    existingContents = reader.ReadToEnd();
+                        //    reader.Close();
+                        //}
+                        //
+                        //// Append new data to the existing contents
+                        //string newData = string.Format("Tick {0}: {1} position: X={2}, Y={3}, Z={4}\n", tickCounter, gridName, currentPosition.X, currentPosition.Y, currentPosition.Z);
+                        //string updatedContents = existingContents + newData;
+                        //
+                        //// Write the updated contents back to the file
+                        //var writer = MyAPIGateway.Utilities.WriteFileInWorldStorage(fileName, typeof(coordoutput));
+                        //writer.Write(updatedContents);
+                        //writer.Flush();
+                        //writer.Close();
+                    }
+                }
+                else
+                {
+                    tickCounter = 0; // Reset tick counter if cockpit is unmanned
+                }
+            }
+        }
+    }
+}
