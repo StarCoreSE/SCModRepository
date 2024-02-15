ed2e933e6ef014eddfdbc3846becca10f66dd1a9
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-02-15 01:06:20 -0600

    Update CustomBlinkDrive.cs

--- SCMobilityBlocks/Data/Scripts/InvalidsCustomBlink/CustomBlinkDrive.cs
+++ SCMobilityBlocks/Data/Scripts/InvalidsCustomBlink/CustomBlinkDrive.cs
@@ -10,7 +10,6 @@ using VRage.Game.Entity;
 using VRage.Game.ModAPI;
 using VRage.Game.ModAPI.Network;
 using VRage.ModAPI;
-using VRage.Network;
 using VRage.ObjectBuilders;
 using VRage.Sync;
 using VRage.Utils;
@@ -19,12 +18,12 @@ using VRageMath;
 namespace Invalid.BlinkDrive
 {
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_UpgradeModule), false, "BlinkDriveCustomLarge")]
-    public class BlinkDrive : MyGameLogicComponent, IMyEventProxy
+    public class BlinkDrive : MyGameLogicComponent
     {
         private IMyCubeBlock block;
         private MySync<bool, SyncDirection.BothWays> requestJumpSync;
         private int jumpCooldownTimer;
-        private const int cooldownDuration = 10 * 60; // Cooldown duration in frames (10 seconds * 60 frames per second)
+        private const int cooldownDuration = 10 * 60; // 10 seconds * 60 frames per second
 
         static bool controlsCreated = false;
 
@@ -34,31 +33,23 @@ namespace Invalid.BlinkDrive
             NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME | MyEntityUpdateEnum.EACH_FRAME;
 
             block = (IMyCubeBlock)Entity;
-
-            MyLog.Default.WriteLineAndConsole("Init method called");
-
+           // requestJumpSync = new MySync<bool, SyncDirection.BothWays>(this, nameof(requestJumpSync));
             requestJumpSync.ValueChanged += RequestJumpSync_ValueChanged;
         }
 
         private void RequestJumpSync_ValueChanged(MySync<bool, SyncDirection.BothWays> obj)
         {
-            MyLog.Default.WriteLineAndConsole("RequestJumpSync_ValueChanged method called");
             if (obj.Value && CanJump())
             {
-                MyLog.Default.WriteLineAndConsole("PerformBlink will be called");
                 if (MyAPIGateway.Multiplayer.IsServer)
                 {
                     PerformBlink();
-                    jumpCooldownTimer = cooldownDuration; // Set cooldown timer
-                }
-                else
-                {
-                    MyLog.Default.WriteLineAndConsole("PerformBlink skipped because not running on server.");
                 }
             }
             else
             {
-                MyLog.Default.WriteLineAndConsole("Value is false or not ready for jump due to cooldown.");
+                // Reset the request on the client side to ensure it's properly synchronized
+                ResetJumpRequest();
             }
         }
 
@@ -69,37 +60,29 @@ namespace Invalid.BlinkDrive
 
         private void PerformBlink()
         {
-            MyLog.Default.WriteLineAndConsole("PerformBlink method called");
             Vector3D forwardVector = block.WorldMatrix.Forward;
             Vector3D teleportPosition = block.CubeGrid.WorldMatrix.Translation + (forwardVector * 1000); // 1km forward
             MatrixD newWorldMatrix = block.CubeGrid.WorldMatrix;
             newWorldMatrix.Translation = teleportPosition;
             (block.CubeGrid as MyEntity).Teleport(newWorldMatrix);
-            MyLog.Default.WriteLineAndConsole("Teleporting to " + teleportPosition.ToString());
-            MyAPIGateway.Utilities.ShowNotification("Blink Drive Activated!", 2000, "Green");
-            requestJumpSync.Value = false;
-            jumpCooldownTimer = 0; // Reset cooldown timer
+
+            // Reset jump request and start cooldown
+            ResetJumpRequest();
+            jumpCooldownTimer = cooldownDuration;
         }
 
         public override void UpdateAfterSimulation()
         {
-              MyAPIGateway.Utilities.ShowNotification("FICL", 2000, "Green");
+            base.UpdateAfterSimulation();
             if (jumpCooldownTimer > 0)
             {
                 jumpCooldownTimer--;
-
-                if (jumpCooldownTimer > 0)
-                {
-                    int secondsRemaining = jumpCooldownTimer / 60; // Convert frames to seconds
-                    MyAPIGateway.Utilities.ShowNotification($"Jump Cooldown: {secondsRemaining} seconds", 1000, "White");
-                }
             }
         }
 
         public override void UpdateOnceBeforeFrame()
         {
             base.UpdateOnceBeforeFrame();
-            MyLog.Default.WriteLineAndConsole("UpdateOnceBeforeFrame method called");
             if (!controlsCreated)
             {
                 CreateTerminalControls();
@@ -130,7 +113,7 @@ namespace Invalid.BlinkDrive
             var blinkDriveCockpitAction = MyAPIGateway.TerminalControls.CreateAction<IMyUpgradeModule>("BlinkDriveActivate");
             blinkDriveCockpitAction.Name = new StringBuilder("Activate Blink Drive");
             blinkDriveCockpitAction.Icon = @"Textures\GUI\Icons\Actions\Start.dds";
-            blinkDriveCockpitAction.Action = b =>
+            blinkDriveCockpitAction.Action = (b) =>
             {
                 var drive = b.GameLogic.GetAs<BlinkDrive>();
                 if (drive != null)
@@ -141,22 +124,34 @@ namespace Invalid.BlinkDrive
             };
             blinkDriveCockpitAction.Writer = (b, sb) =>
             {
-                sb.Append("Activates the Blink Drive for a single jump.");
+                var drive = b.GameLogic.GetAs<BlinkDrive>();
+                if (drive != null)
+                {
+                    int secondsRemaining = Math.Max(drive.jumpCooldownTimer / 60, 0); // Convert frames to seconds
+                    sb.Append($"CD {secondsRemaining} s");
+                }
+                else
+                {
+                    sb.Append("Unable to retrieve cooldown information.");
+                }
             };
-            blinkDriveCockpitAction.Enabled = b => b.GameLogic is BlinkDrive;
+            blinkDriveCockpitAction.Enabled = (b) => b.GameLogic is BlinkDrive;
 
             MyAPIGateway.TerminalControls.AddAction<IMyUpgradeModule>(blinkDriveCockpitAction);
+        }
+
+        private void ResetJumpRequest()
+        {
+            // This ensures the jump request is reset correctly on both server and clients
+            requestJumpSync.Value = false;
         }
 
         public override void Close()
         {
             base.Close();
-            MyLog.Default.WriteLineAndConsole("Close method called");
-
             if (requestJumpSync != null)
             {
                 requestJumpSync.ValueChanged -= RequestJumpSync_ValueChanged;
-                requestJumpSync = null;
             }
         }
     }
