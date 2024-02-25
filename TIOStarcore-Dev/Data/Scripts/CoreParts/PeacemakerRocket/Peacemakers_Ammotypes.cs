666bc33c416391a76460546eccc5112911f071a3
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-02-25 18:57:25 -0500

    Name Updates
    Next push will be lasers

--- TIOStarcore-Dev/Data/Scripts/CoreParts/PeacemakerRocket/Peacemakers_Ammotypes.cs
+++ TIOStarcore-Dev/Data/Scripts/CoreParts/PeacemakerRocket/Peacemakers_Ammotypes.cs
@@ -194,11 +194,11 @@ namespace Scripts
                 EndOfLife = new EndOfLifeDef
                 {
                     Enable = true,
-                    Radius = 9f, // Meters
+                    Radius = 8f, // Meters
                     Damage = 16500,
-                    Depth = 3.5f,
+                    Depth = 3f,
                     MaxAbsorb = 0f,
-                    Falloff = Linear, //.NoFalloff applies the same damage to all blocks in radius
+                    Falloff = Pooled, //.NoFalloff applies the same damage to all blocks in radius
                     //.Linear drops evenly by distance from center out to max radius
                     //.Curve drops off damage sharply as it approaches the max radius
                     //.InvCurve drops off sharply from the middle and tapers to max radius
@@ -283,11 +283,11 @@ namespace Scripts
                 TargetLossTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 MaxLifeTime = 200, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..). Please have a value for this, It stops Bad things.
                 AccelPerSec = 0f, // Meters Per Second. This is the spawning Speed of the Projectile, and used by turning.
-                DesiredSpeed = 1900, // voxel phasing if you go above 5100
-                MaxTrajectory = 6500f, // Max Distance the projectile or beam can Travel.
+                DesiredSpeed = 900, // voxel phasing if you go above 5100
+                MaxTrajectory = 2500f, // Max Distance the projectile or beam can Travel.
                 DeaccelTime = 0, // 0 is disabled, a value causes the projectile to come to rest overtime, (Measured in game ticks, 60 = 1 second)
                 GravityMultiplier = 0f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable. Natural Gravity Only.
-                SpeedVariance = Random(start: 0, end: 0), // subtracts value from DesiredSpeed. Be warned, you can make your projectile go backwards.
+                SpeedVariance = Random(start: -300, end: 300), // subtracts value from DesiredSpeed. Be warned, you can make your projectile go backwards.
                 RangeVariance = Random(start: 250, end: 500), // subtracts value from MaxTrajectory
                 MaxTrajectoryTime = 0, // How long the weapon must fire before it reaches MaxTrajectory.
                 TotalAcceleration = 0, // 0 means no limit, something to do due with a thing called delta and something called v.
