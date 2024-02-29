6de86b15b4ff35dcad86967bc5b99109ecc05be4
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-02-29 11:44:00 -0500

    Breaking the Habit (Fixing the Patchnotes)
    TIO LIVE version resists to 300%.

--- TIOStarcore/Data/Scripts/CoreParts/Type79_Railgun_Part.cs
+++ TIOStarcore/Data/Scripts/CoreParts/Type79_Railgun_Part.cs
@@ -25,7 +25,7 @@ namespace Scripts {
                         MuzzlePartId = "Mk3_RailgunBarrels", // The subpart where your muzzle empties are located. This is often the elevation subpart. Subpart_Boomsticks must be written as Boomsticks.
                         AzimuthPartId = "Mk3_RailgunAZ", // Your Rotating Subpart, the bit that moves sideways.
                         ElevationPartId = "Mk3_RailgunElevation",// Your Elevating Subpart, that bit that moves up.
-                        DurabilityMod = 0.15f, // GeneralDamageMultiplier, 0.25f = 25% damage taken.
+                        DurabilityMod = 0.25f, // GeneralDamageMultiplier, 0.25f = 25% damage taken.
                         IconName = "TestIcon.dds" // Overlay for block inventory slots, like reactors, refineries, etc.
                     },
                     
