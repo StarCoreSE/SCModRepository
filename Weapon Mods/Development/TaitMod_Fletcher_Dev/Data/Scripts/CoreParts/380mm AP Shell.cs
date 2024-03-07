5f1406bd3e9b140e149d816307175b6fccdcefac
TaitKaito <clayton.h.c123@gmail.com>
2024-03-07 23:49:57 +0000

    ammo range fix
    15 inch Ammo (used by 16s as well): 6000 - 10000

--- Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/380mm AP Shell.cs
+++ Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/380mm AP Shell.cs
@@ -242,7 +242,7 @@ namespace Scripts
                 MaxLifeTime = 2000, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..). Please have a value for this, It stops Bad things.
                 AccelPerSec = 0f, // Meters Per Second. This is the spawning Speed of the Projectile, and used by turning.
                 DesiredSpeed = 1300, // voxel phasing if you go above 5100
-                MaxTrajectory = 8000f, // Max Distance the projectile or beam can Travel.
+                MaxTrajectory = 10000f, // Max Distance the projectile or beam can Travel.
                 DeaccelTime = 0, // 0 is disabled, a value causes the projectile to come to rest, spawn a field and remain for a time (Measured in game ticks, 60 = 1 second)
                 GravityMultiplier = 3f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable. Natural Gravity Only.
                 SpeedVariance = Random(start: 0, end: 0), // subtracts value from DesiredSpeed. Be warned, you can make your projectile go backwards.
