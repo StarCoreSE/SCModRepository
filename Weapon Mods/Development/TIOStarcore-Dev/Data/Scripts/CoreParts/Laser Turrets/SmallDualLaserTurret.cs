764a7816589216ffaebc97ba7ff79128aae4b42e
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-03-07 22:50:05 -0500

    Small Lasers and Reavers
    Small Lasers and Reaver/Raider block clones.

--- Weapon Mods/Development/TIOStarcore-Dev/Data/Scripts/CoreParts/Laser Turrets/SmallDualLaserTurret.cs
+++ Weapon Mods/Development/TIOStarcore-Dev/Data/Scripts/CoreParts/Laser Turrets/SmallDualLaserTurret.cs
@@ -124,7 +124,7 @@ namespace Scripts {
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
                     ReloadTime = 240, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 0, // Number of physical magazines to consume on reload.
-                    DelayUntilFire = 10, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    DelayUntilFire = 30, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 0, // Heat generated per shot.
                     MaxHeat = 500, // Max heat before weapon enters cooldown (70% of max heat).
                     Cooldown = .95f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
