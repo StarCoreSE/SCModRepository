00533c4eca7cf6a8409f750e303de0569b11255e
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-03-08 20:16:39 -0500

    Keep Eye on Rlx HP
    is 92, erebos do 150+ in a 45m aoe, should keep an eye on that...

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/FixedgunRLXammo.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/FixedgunRLXammo.cs
@@ -439,7 +439,7 @@ namespace Scripts
             EnergyCost = 0f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
             BaseDamage = 10000f, // Direct damage; one steel plate is worth 100.
             Mass = 140f, // In kilograms; how much force the impact will apply to the target.
-            Health = 92, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
+            Health = 92, //Watch this against Erebos 100+ damage aoe in future!// How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
             BackKickForce = 0f, // Recoil. This is applied to the Parent Grid.
             DecayPerShot = 0f, // Damage to the firing weapon itself. 
                                //float.MaxValue will drop the weapon to the first build state and destroy all components used for construction
