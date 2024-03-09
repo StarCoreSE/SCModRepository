e6bbee5b694df212e068a3459dcd9e818acfd905
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-03-09 03:07:36 -0500

    I think somethings gone fucky somewhere
    ive switched too many branches and I dont know what i did wrong.
    
    Pushing this cause the turret shouldnt be able to fucking work without firefull but it does in combat 1 so IDK man
    
    also reload was changed but then lost in a commit stash somehow reeee.

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/.TypeRails/Type25 Rail Turret.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/.TypeRails/Type25 Rail Turret.cs
@@ -156,7 +156,7 @@ namespace Scripts {
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 240, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 390, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 3, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 105, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 30, // Heat generated per shot.
@@ -166,7 +166,7 @@ namespace Scripts {
                     DegradeRof = false, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 0, // Use this if you don't want the weapon to fire an entire physical magazine in one go. Should not be more than your magazine capacity.
                     DelayAfterBurst = 0, // How long to spend "reloading" after each burst. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    FireFull = false, // Whether the weapon should fire the full magazine (or the full burst instead if ShotsInBurst > 0), even if the target is lost or the player stops firing prematurely.
+                    FireFull = true, // Whether the weapon should fire the full magazine (or the full burst instead if ShotsInBurst > 0), even if the target is lost or the player stops firing prematurely.
                     GiveUpAfter = false, // Whether the weapon should drop its current target and reacquire a new target after finishing its magazine or burst.
                     BarrelSpinRate = 0, // Visual only, 0 disables and uses RateOfFire.
                     DeterministicSpin = false, // Spin barrel position will always be relative to initial / starting positions (spin will not be as smooth).
