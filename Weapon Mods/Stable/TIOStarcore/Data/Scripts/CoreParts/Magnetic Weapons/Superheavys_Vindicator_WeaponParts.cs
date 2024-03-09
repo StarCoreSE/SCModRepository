98d6f5dacf843a1e4f47b03a038145adf2e45a39
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-03-08 21:00:59 -0500

    Rotation and Power tweaks
     after a scrim with the weapons, some minor passive power adjustments, and some rotational values changed to make the pack more consistent with itself

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Magnetic Weapons/Superheavys_Vindicator_WeaponParts.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Magnetic Weapons/Superheavys_Vindicator_WeaponParts.cs
@@ -118,8 +118,8 @@ namespace Scripts {
                 },
                 HardWare = new HardwareDef
                 {
-                    RotateRate = 0.0019f, // Max traversal speed of azimuth subpart in radians per tick (0.1 is approximately 360 degrees per second).
-                    ElevateRate = 0.001f, // Max traversal speed of elevation subpart in radians per tick.
+                    RotateRate = 0.0017f, // Max traversal speed of azimuth subpart in radians per tick (0.1 is approximately 360 degrees per second).
+                    ElevateRate = 0.0011f, // Max traversal speed of elevation subpart in radians per tick.
                     MinAzimuth = -180,
                     MaxAzimuth = 180,
                     MinElevation = -10,
