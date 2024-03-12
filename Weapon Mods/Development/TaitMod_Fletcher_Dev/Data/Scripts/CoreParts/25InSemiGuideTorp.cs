d3493dbcae921704f58f40f7d371e68dcf8fc782
TaitKaito <clayton.h.c123@gmail.com>
2024-03-12 15:16:42 +0000

    all stages!!

--- Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/25InSemiGuideTorp.cs
+++ Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/25InSemiGuideTorp.cs
@@ -131,7 +131,7 @@ namespace Scripts
                 },
                 Shields = new ShieldDef
                 {
-                    Modifier = 4f, // Multiplier for damage against shields.
+                    Modifier = 1f, // Multiplier for damage against shields.
                     Type = Default, // Damage vs healing against shields; Default, Heal
                     BypassModifier = -1f, // If greater than zero, the percentage of damage that will penetrate the shield.
                 },
@@ -519,7 +519,7 @@ namespace Scripts
                 },
                 Shields = new ShieldDef
                 {
-                    Modifier = 4f, // Multiplier for damage against shields.
+                    Modifier = 1f, // Multiplier for damage against shields.
                     Type = Default, // Damage vs healing against shields; Default, Heal
                     BypassModifier = -1f, // If greater than zero, the percentage of damage that will penetrate the shield.
                 },
