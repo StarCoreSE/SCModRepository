d440436923c5a42f36e2c22c9055b9e95efc3322
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-03-12 23:53:40 -0400

    hmm max turn set to 1? sus
    jdgkhavn ubkryAvNIAH setting lateral thrust to 0 on first stage

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/FixedgunRLXammo.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/FixedgunRLXammo.cs
@@ -290,8 +290,8 @@ namespace Scripts
                 Smarts = new SmartsDef
                 {
                     Inaccuracy = 0f, // 0 is perfect, hit accuracy will be a random num of meters between 0 and this value.
-                    Aggressiveness = 3f, // controls how responsive tracking is.
-                    MaxLateralThrust = 1, // controls how sharp the trajectile may turn
+                    Aggressiveness = 0f, // controls how responsive tracking is.
+                    MaxLateralThrust = 0, // controls how sharp the trajectile may turn
                     TrackingDelay = 1000, // Measured in Shape diameter units traveled.
                     MaxChaseTime = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     OverideTarget = false, // when set to true ammo picks its own target, does not use hardpoint's.
