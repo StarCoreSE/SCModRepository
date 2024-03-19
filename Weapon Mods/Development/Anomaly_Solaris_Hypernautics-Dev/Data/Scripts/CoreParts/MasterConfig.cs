36382b58e6bb5622ed7ebc9d3ec5a7de39cd69f1
FP-Anomaly <carbonjvd@gmail.com>
2024-03-18 20:31:02 -0700

    WIP Weapons added to Dev
    4 new weapons
     * SM-3 Counter Battery (AMS Missile Launcher)
    * ArcStrike CS-LR Torpedo (Shield Buster Missile Launcher)
    * Hercules MAC (FCC masquerading as a MAC :^)
    * Saber Chemical Laser (Small Powerful Laser with Limited Ammo)

---
+++ Weapon Mods/Development/Anomaly_Solaris_Hypernautics-Dev/Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            PartDefinitions(
+		AMP_FieldGenerator,
+		AMP_ImpulseTorch,
+		AMP_FlakShotgun,
+                S_Chem_Laser,
+		SM3_Counter_Battery,
+		SolHyp_ArcStrike_HTorpedo,
+                SolHyp_MAC
+	    );
+        }
+    }
+}
