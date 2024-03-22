bc8f547c9e568ad0e8a27b7794a13c146930176c
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-03-22 16:04:19 -0400

    Type 21 Updated and DD updated
    Type 21 Updated and Dakka Dossier updated to reflect what type 18 & 24 were doing in game, after in game testing.
    
    Type 21 was updated slightly to make sure it was exactly in the middle of the type 18 and 24.

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/.TypeCannons/Type18 Heavy Turret.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/.TypeCannons/Type18 Heavy Turret.cs
@@ -125,7 +125,7 @@ namespace Scripts {
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 1200, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 1200, //20 seconds //Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 8, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 45, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 10, // Heat generated per shot.
