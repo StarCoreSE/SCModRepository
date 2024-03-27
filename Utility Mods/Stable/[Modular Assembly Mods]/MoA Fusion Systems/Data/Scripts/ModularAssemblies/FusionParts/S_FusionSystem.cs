f65dff79665c505a5d911d2ce81bb160cc013a3a
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-03-26 20:02:04 -0500

    add many null checks (pain)

--- Utility Mods/Stable/[Modular Assembly Mods]/MoA Fusion Systems/Data/Scripts/ModularAssemblies/FusionParts/S_FusionSystem.cs
+++ Utility Mods/Stable/[Modular Assembly Mods]/MoA Fusion Systems/Data/Scripts/ModularAssemblies/FusionParts/S_FusionSystem.cs
@@ -35,40 +35,46 @@ namespace Scripts.ModularAssemblies.FusionParts
 
         public void AddPart(IMyCubeBlock newPart)
         {
-            // Scan for 'arms' connected on both ends to the feeder block.
-            switch (newPart.BlockDefinition.SubtypeName)
+            if (newPart != null)
             {
-                case "Caster_Accelerator_0":
-                case "Caster_Accelerator_90":
-                    S_FusionArm newArm = new S_FusionArm((MyEntity) newPart, "Caster_Feeder");
-                    if (newArm.IsValid)
-                        Arms.Add(newArm);
-                    break;
+                // Scan for 'arms' connected on both ends to the feeder block.
+                switch (newPart.BlockDefinition.SubtypeName)
+                {
+                    case "Caster_Accelerator_0":
+                    case "Caster_Accelerator_90":
+                        S_FusionArm newArm = new S_FusionArm((MyEntity)newPart, "Caster_Feeder");
+                        if (newArm.IsValid)
+                            Arms.Add(newArm);
+                        break;
+                }
+
+                if (newPart is IMyThrust)
+                    Thrusters.Add((IMyThrust)newPart);
+
+                if (newPart is IMyReactor)
+                    Reactors.Add((IMyReactor)newPart);
+
+                UpdatePower();
             }
-
-            if (newPart is IMyThrust)
-                Thrusters.Add((IMyThrust) newPart);
-
-            if (newPart is IMyReactor)
-                Reactors.Add((IMyReactor)newPart);
-
-            UpdatePower();
         }
 
         public void RemovePart(IMyCubeBlock part)
         {
-            if (part is IMyThrust)
-                Thrusters.Remove((IMyThrust) part);
-            if (part is IMyReactor)
-                Reactors.Remove((IMyReactor) part);
-
-            foreach (var arm in Arms)
+            if (part != null)
             {
-                if (arm.Parts.Contains(part))
+                if (part is IMyThrust)
+                    Thrusters.Remove((IMyThrust)part);
+                if (part is IMyReactor)
+                    Reactors.Remove((IMyReactor)part);
+
+                foreach (var arm in Arms)
                 {
-                    Arms.Remove(arm);
-                    UpdatePower();
-                    break;
+                    if (arm.Parts?.Contains(part) == true)
+                    {
+                        Arms.Remove(arm);
+                        UpdatePower();
+                        break;
+                    }
                 }
             }
         }
@@ -81,32 +87,38 @@ namespace Scripts.ModularAssemblies.FusionParts
 
             foreach (var arm in Arms)
             {
-                PowerGeneration += arm.PowerGeneration;
-                PowerCapacity += arm.PowerStorage;
+
+                    PowerGeneration += arm.PowerGeneration;
+                    PowerCapacity += arm.PowerStorage;
+
             }
 
             // Math for slider on reactor parts to allow for a power <-> efficiency tradeoff.
             foreach (var reactor in Reactors)
             {
-                if (reactor.BlockDefinition.SubtypeName == "Caster_Controller")
+                if (reactor?.BlockDefinition.SubtypeName == "Caster_Controller")
                     continue;
 
-                // Temporary percentage of fusion output to use. Should be slider.
-                float temp_reactorConsumptionMultiplier = reactor.GameLogic.GetAs<FusionReactorLogic>().PowerUsageSync.Value; // This is ugly, let's make it better.
-                float reactorEfficiencyMultiplier = 1 / (0.5f + temp_reactorConsumptionMultiplier);
+                var fusionReactorLogic = reactor?.GameLogic?.GetAs<FusionReactorLogic>();
+                if (fusionReactorLogic != null)
+                {
+                    // Temporary percentage of fusion output to use. Should be slider.
+                    float temp_reactorConsumptionMultiplier = fusionReactorLogic.PowerUsageSync.Value; // This is ugly, let's make it better.
+                    float reactorEfficiencyMultiplier = 1 / (0.5f + temp_reactorConsumptionMultiplier);
 
-                // Power generation consumed (per second)
-                float powerConsumption = PowerGeneration * 60 * temp_reactorConsumptionMultiplier;
-                // Power generated (per second)
-                float reactorOutput = reactorEfficiencyMultiplier * powerConsumption * MegawattsPerFusionPower;
+                    // Power generation consumed (per second)
+                    float powerConsumption = PowerGeneration * 60 * temp_reactorConsumptionMultiplier;
+                    // Power generated (per second)
+                    float reactorOutput = reactorEfficiencyMultiplier * powerConsumption * MegawattsPerFusionPower;
 
-                MyAPIGateway.Utilities.ShowNotification($"Output: {Math.Round(reactorOutput, 1)}/{Math.Round(PowerGeneration*60*MegawattsPerFusionPower, 1)}", 1000/60);
-                MyAPIGateway.Utilities.ShowNotification($"Input: {Math.Round(powerConsumption, 1)}/{Math.Round(PowerGeneration*60, 1)}", 1000 / 60);
-                MyAPIGateway.Utilities.ShowNotification($"Efficiency: {Math.Round(reactorEfficiencyMultiplier*100)}%", 1000 / 60);
+                    MyAPIGateway.Utilities?.ShowNotification($"Output: {Math.Round(reactorOutput, 1)}/{Math.Round(PowerGeneration * 60 * MegawattsPerFusionPower, 1)}", 1000 / 60);
+                    MyAPIGateway.Utilities?.ShowNotification($"Input: {Math.Round(powerConsumption, 1)}/{Math.Round(PowerGeneration * 60, 1)}", 1000 / 60);
+                    MyAPIGateway.Utilities?.ShowNotification($"Efficiency: {Math.Round(reactorEfficiencyMultiplier * 100)}%", 1000 / 60);
 
-                // Convert back into power per tick
-                totalPowerUsage += powerConsumption / 60;
-                SyncMultipliers.ReactorOutput(reactor, reactorOutput);
+                    // Convert back into power per tick
+                    totalPowerUsage += powerConsumption / 60;
+                    SyncMultipliers.ReactorOutput(reactor, reactorOutput);
+                }
             }
 
             // Subtract power usage afterwards so that all reactors have the same stats.
