4103fc669cbc357dc7a9a79c37bb53428c4382f3
TaitKaito <clayton.h.c123@gmail.com>
2024-03-28 13:35:11 +0000

    He shell shrapnel now has falloff

--- Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/380mm HE Shell.cs
+++ Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/380mm HE Shell.cs
@@ -445,8 +445,8 @@ namespace Scripts
                 // For the following modifier values: -1 = disabled (higher performance), 0 = no damage, 0.01f = 1% damage, 2 = 200% damage.
                 FallOff = new FallOffDef
                 {
-                    Distance = 0f, // Distance at which damage begins falling off.
-                    MinMultipler = 0f, // Value from 0.0001f to 1f where 0.1f would be a min damage of 10% of base damage.
+                    Distance = 5, // Distance at which damage begins falling off.
+                    MinMultipler = 0.05f, // Value from 0.0001f to 1f where 0.1f would be a min damage of 10% of base damage.
                 },
                 Grids = new GridSizeDef
                 {
