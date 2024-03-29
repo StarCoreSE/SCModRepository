9078605f22830249b89fa8cfa2b50022dceba066
TaitKaito <clayton.h.c123@gmail.com>
2024-03-29 02:12:18 +0000

    16 inch love tap
    16 inch triple
    	reload: 630-570
    	rate of fire: 400-550

--- Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/406mmalt.cs
+++ Weapon Mods/Development/TaitMod_Fletcher_Dev/Data/Scripts/CoreParts/406mmalt.cs
@@ -125,11 +125,11 @@ namespace Scripts {
                 },
                 Loading = new LoadingDef
                 {
-                    RateOfFire = 400, // Set this to 3600 for beam weapons. This is how fast your Gun fires.
+                    RateOfFire = 550, // Set this to 3600 for beam weapons. This is how fast your Gun fires.
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 630, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 570, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 3, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 0, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 1, // Heat generated per shot.
