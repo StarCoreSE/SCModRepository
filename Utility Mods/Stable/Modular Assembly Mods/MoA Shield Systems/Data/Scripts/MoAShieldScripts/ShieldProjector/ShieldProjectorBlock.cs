69d68423bcf10f7d7a0067b21a562dc6a1ddb53c
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-04-18 23:22:04 -0500

    setup

---
+++ Utility Mods/Stable/Modular Assembly Mods/MoA Shield Systems/Data/Scripts/MoAShieldScripts/ShieldProjector/ShieldProjectorBlock.cs
@@ -0,0 +1,111 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using System;
+using System.Collections.Generic;
+using Sandbox.Game.EntityComponents;
+
+namespace Invalid.ShieldProjector
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_UpgradeModule), false, "ShieldProjector_Large")]
+    public class ShieldProjector : MyGameLogicComponent
+    {
+        private IMyCubeBlock block;
+        private ShieldProjectorSettings Settings;
+
+        public readonly Guid FoamSettingsGUID = new Guid("6200280a-be69-48ed-afa9-cf376ea62b8b");
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            base.Init(objectBuilder);
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+            block = (IMyCubeBlock)Entity;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            base.UpdateOnceBeforeFrame();
+            CreateTerminalControls();
+
+            if (block == null || block.CubeGrid?.Physics == null)
+                return;
+
+            Settings = new ShieldProjectorSettings(this);
+
+            LoadSettings();
+            SaveSettings();
+        }
+
+        static void CreateTerminalControls()
+        {
+            // Add your terminal control creation logic here
+        }
+
+        internal bool LoadSettings()
+        {
+            string rawData;
+            if (block.Storage == null || !block.Storage.TryGetValue(FoamSettingsGUID, out rawData))
+            {
+                return false;
+            }
+
+            try
+            {
+                var loadedSettings = MyAPIGateway.Utilities.SerializeFromBinary<ShieldProjectorSettings>(Convert.FromBase64String(rawData));
+                if (loadedSettings == null)
+                    return false;
+
+                // Assign loaded settings to the current settings
+                Settings.FoamRadius = loadedSettings.FoamRadius;
+                Settings.IsFoaming = loadedSettings.IsFoaming;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                MyAPIGateway.Utilities.ShowNotification("Failed to load foam settings! Check the logs for more info.");
+                MyLog.Default.WriteLineAndConsole("Failed to load foam settings! Exception: " + e);
+            }
+
+            return false;
+        }
+
+        internal void SaveSettings()
+        {
+            if (block == null || Settings == null)
+                return;
+
+            if (block.Storage == null)
+                block.Storage = new MyModStorageComponent();
+
+            string rawData = Convert.ToBase64String(MyAPIGateway.Utilities.SerializeToBinary(Settings));
+            block.Storage.Add(FoamSettingsGUID, rawData);
+        }
+
+        public override bool IsSerialized()
+        {
+            try
+            {
+                SaveSettings();
+            }
+            catch (Exception e)
+            {
+                // Log any exceptions that occur during serialization
+            }
+
+            return base.IsSerialized();
+        }
+
+        public override void Close()
+        {
+            base.Close();
+            // Add any cleanup logic here
+        }
+    }
+}
---
+++ Utility Mods/Stable/Modular Assembly Mods/MoA Shield Systems/Data/Scripts/MoAShieldScripts/ShieldProjector/ShieldProjectorSettings.cs
@@ -0,0 +1,54 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.ModAPI.Network;
+using VRage.Sync;
+using VRage.Utils;
+
+namespace Invalid.ShieldProjector
+{
+    [ProtoContract]
+    internal class ShieldProjectorSettings
+    {
+        ShieldProjectorSettings() { }
+        public ShieldProjectorSettings(ShieldProjector owner)
+        {
+            Owner = owner;
+           // Owner.IsFoamingSync.ValueChanged += (obj) => isFoaming = obj.Value;
+           // Owner.FoamRadiusSync.ValueChanged += (obj) => foamRadius = obj.Value;
+        }
+
+        [ProtoMember(1)] private bool isFoaming;
+        [ProtoMember(2)] private float foamRadius;
+        ShieldProjector Owner;
+
+        public bool IsFoaming
+        {
+            get
+            {
+                return isFoaming;
+            }
+            set
+            {
+                isFoaming = value;
+              //  Owner.IsFoamingSync.Value = value;
+            }
+        }
+
+        public float FoamRadius
+        {
+            get
+            {
+                return foamRadius;
+            }
+            set
+            {
+                foamRadius = value;
+               // Owner.FoamRadiusSync.Value = value;
+            }
+        }
+    }
+}
