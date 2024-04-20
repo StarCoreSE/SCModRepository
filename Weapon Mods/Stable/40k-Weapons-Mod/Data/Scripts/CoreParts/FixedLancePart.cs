c67fc3327ae259d0106929e56ea3400d931f5e4c
Darth411 <darth411@gmail.com>
2024-04-20 00:55:14 -0400

    Fixes and Points to Stable

--- Weapon Mods/Stable/40k-Weapons-Mod/Data/Scripts/CoreParts/FixedLancePart.cs
+++ Weapon Mods/Stable/40k-Weapons-Mod/Data/Scripts/CoreParts/FixedLancePart.cs
@@ -164,7 +164,7 @@ namespace Scripts {
                     DegradeRof = false, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 0, // Use this if you don't want the weapon to fire an entire physical magazine in one go. Should not be more than your magazine capacity.
                     DelayAfterBurst = 0, // How long to spend "reloading" after each burst. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    FireFull = false, // Whether the weapon should fire the full magazine (or the full burst instead if ShotsInBurst > 0), even if the target is lost or the player stops firing prematurely.
+                    FireFull = true, // Whether the weapon should fire the full magazine (or the full burst instead if ShotsInBurst > 0), even if the target is lost or the player stops firing prematurely.
                     GiveUpAfter = false, // Whether the weapon should drop its current target and reacquire a new target after finishing its magazine or burst.
                     BarrelSpinRate = 0, // Visual only, 0 disables and uses RateOfFire.
                     DeterministicSpin = false, // Spin barrel position will always be relative to initial / starting positions (spin will not be as smooth).
