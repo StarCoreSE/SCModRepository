4aea7495cf55068d06fd172db607b97e0c6fc512
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-04-26 18:26:55 -0500

    Update CleanModCategories.cs

--- Utility Mods/Stable/SCCleanModCategories/Data/Scripts/CleanModCategories.cs
+++ Utility Mods/Stable/SCCleanModCategories/Data/Scripts/CleanModCategories.cs
@@ -38,13 +38,14 @@ namespace Jakaria
                 }
                 else if (!definition.Value.Context.IsBaseGame &&
                          definition.Value.Name != "DLCBlocks" &&
-                       /*definition.Value.Name != ".SC Tournament Weapons" && */
+                         /*definition.Value.Name != ".SC Tournament Weapons" && */
                          definition.Value.Name != ".Starcore Basic Greebles" &&
                          definition.Value.Name != "Fusion Systems" &&
-                       /*definition.Value.Name != ".SC WIP" && */
+                         /*definition.Value.Name != ".SC WIP" && */
                          definition.Value.Name != "[All Access]" &&
-                       /*definition.Value.Name != ".Ship Cores" && */
-                       /*definition.Value.Name != ".SC Vanilla 2.0 Weapons" && */
+                        /*definition.Value.Name != ".Ship Cores" && */
+                        /*definition.Value.Name != ".SC Vanilla 2.0 Weapons" && */
+                         definition.Value.Name != "[40K]" &&               
                          definition.Value.Name != "[TIO]" &&
                          definition.Value.Name != "[MID]" &&
                          definition.Value.Name != "[MA]" &&
@@ -53,7 +54,7 @@ namespace Jakaria
                          definition.Value.Name != "[SA]" &&
                          definition.Value.Name != "[HAS]" &&
                          definition.Value.Name != "[SOL]" &&
-						 definition.Value.Name != "[FAS]" &&
+                         definition.Value.Name != "[FAS]" &&
                          definition.Value.Name != "[EXO]" &&
                          definition.Value.Name != "[BTI]")
                 {
