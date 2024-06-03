0a8e19e24efd57b3e1135f5c5d29ea93301d0440
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-06-03 12:32:01 -0500

    yep there's definitely changes

--- Weapon Mods/Stable/Starcore-MA-Weapons-Natemod/Data/Scripts/CoreParts/MA_Laser_T1_Ammo.cs
+++ Weapon Mods/Stable/Starcore-MA-Weapons-Natemod/Data/Scripts/CoreParts/MA_Laser_T1_Ammo.cs
@@ -110,8 +110,8 @@ namespace Scripts
                 // For the following modifier values: -1 = disabled (higher performance), 0 = no damage, 0.01f = 1% damage, 2 = 200% damage.
                 FallOff = new FallOffDef
                 {
-                    Distance = 800f, // Distance at which damage begins falling off.
-                    MinMultipler = 0.1f, // Value from 0.0001f to 1f where 0.1f would be a min damage of 10% of base damage.
+                    Distance = 1000f, // Distance at which damage begins falling off.
+                    MinMultipler = 0.33f, // Value from 0.0001f to 1f where 0.1f would be a min damage of 10% of base damage.
                 },
                 Grids = new GridSizeDef
                 {
@@ -266,7 +266,7 @@ namespace Scripts
                 MaxLifeTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 AccelPerSec = 0f,
                 DesiredSpeed = 30000, // voxel phasing if you go above 5100
-                MaxTrajectory = 1200f,
+                MaxTrajectory = 1500f,
                 //FieldTime = 0, // 0 is disabled, a value causes the projectile to come to rest, spawn a field and remain for a time (Measured in game ticks, 60 = 1 second)
                 GravityMultiplier = 0f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable.
                 SpeedVariance = Random(start: 0, end: 0), // subtracts value from DesiredSpeed
