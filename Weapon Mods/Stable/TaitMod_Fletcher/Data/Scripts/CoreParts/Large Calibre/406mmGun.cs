c1245bdbc27b2801f38a1e2961bac96ccd737a98
TaitKaito <clayton.h.c123@gmail.com>
2024-07-10 17:36:41 +0100

    Bug fix patch
    Bug fix patch:
    Fixed missing iowa firing sound
    New sounds turned down.... vaccuum world sounds find, starcore world they get more PUNCHY, also limited how many sounds play at once

--- Weapon Mods/Stable/TaitMod_Fletcher/Data/Scripts/CoreParts/Large Calibre/406mmGun.cs
+++ Weapon Mods/Stable/TaitMod_Fletcher/Data/Scripts/CoreParts/Large Calibre/406mmGun.cs
@@ -151,7 +151,7 @@ namespace Scripts {
                 Audio = new HardPointAudioDef
                 {
                     PreFiringSound = "", // Audio for warmup effect.
-                    FiringSound = "", // Audio for firing.
+                    FiringSound = "FAHeavyShot", // Audio for firing.
                     FiringSoundPerShot = true, // Whether to replay the sound for each shot, or just loop over the entire track while firing.
                     ReloadSound = "", // Sound SubtypeID, for when your Weapon is in a reloading state
                     NoAmmoSound = "",
