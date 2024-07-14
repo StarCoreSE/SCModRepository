02aeb99f4c4b94485c37fe101dd183966a3066c6
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-07-13 20:56:19 -0500

    Create StyleCounter

---
+++ Visual Mods/StyleCounter/Data/Scripts/StyleCounter/MainSession.cs
@@ -0,0 +1,9 @@
+﻿using VRage.Game.Components;
+
+namespace StarCore.StyleCounter
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    public class MainSession : MySessionComponentBase
+    {
+    }
+}
