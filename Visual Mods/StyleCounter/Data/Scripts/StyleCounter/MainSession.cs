72296c5a3272cd7aa2f5521dd00806b094825b75
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-07-13 22:41:24 -0500

    Basic HUD setup

--- Visual Mods/StyleCounter/Data/Scripts/StyleCounter/MainSession.cs
+++ Visual Mods/StyleCounter/Data/Scripts/StyleCounter/MainSession.cs
@@ -1,9 +1,39 @@
-﻿using VRage.Game.Components;
+﻿using RichHudFramework.Client;
+using RichHudFramework.UI.Client;
+using VRage.Game.Components;
 
 namespace StarCore.StyleCounter
 {
     [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
     public class MainSession : MySessionComponentBase
     {
+        internal StyleCounterHud Hud;
+
+        #region Base Methods
+
+        public override void LoadData()
+        {
+            RichHudClient.Init(ModContext.ModName, InitDraw, null);
+        }
+
+        protected override void UnloadData()
+        {
+
+        }
+
+        public override void Draw()
+        {
+            if (!RichHudClient.Registered)
+                return;
+
+
+        }
+
+        #endregion
+
+        private void InitDraw()
+        {
+            Hud = new StyleCounterHud(HudMain.HighDpiRoot);
+        }
     }
 }
---
+++ Visual Mods/StyleCounter/Data/Scripts/StyleCounter/StyleCounterHud.cs
@@ -0,0 +1,29 @@
+﻿using RichHudFramework.UI;
+using RichHudFramework.UI.Client;
+using RichHudFramework.UI.Rendering;
+using Sandbox.ModAPI;
+using VRageMath;
+using VRageRender;
+
+namespace StarCore.StyleCounter
+{
+    internal class StyleCounterHud : CamSpaceNode
+    {
+        // TODO: Only show if Style Meter:tm: is above 0
+
+        private readonly TexturedBox _backgroundBox;
+
+        public StyleCounterHud(HudParentBase parent) : base(parent)
+        {
+            _backgroundBox = new TexturedBox(this)
+            {
+                Material = new Material("CounterBackground", new Vector2(500, 300)),
+                Size = new Vector2(400, 240),
+            };
+
+            RotationAxis = new Vector3(0, -1, 0);
+            RotationAngle = 0.4f;
+            TransformOffset = new Vector3D(0.06, -0.03, -0.05);
+        }
+    }
+}
