0965bab62b067a90989fe03b72732a4415820591
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-07-20 14:27:27 -0400

    Heat doubled, thanks natdawg

--- Weapon Mods/Stable/Starcore_Serpent_Arms_Heavy_Metal/Data/Scripts/CoreParts/Hellfire_Laser_Ammo.cs
+++ Weapon Mods/Stable/Starcore_Serpent_Arms_Heavy_Metal/Data/Scripts/CoreParts/Hellfire_Laser_Ammo.cs
@@ -37,7 +37,7 @@ namespace Scripts
             AmmoRound = "Hellfire Laser", // Name of ammo in terminal, should be different for each ammo type used by the same weapon.
             HybridRound = false, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 0.20f, //was 0.40 //draws 600MW //Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
-            BaseDamage = 1500f, // Direct damage; one steel plate is worth 100. 1
+            BaseDamage = 1500f, // Direct damage; one steel plate is worth 100.
             Mass = 0f, // In kilograms; how much force the impact will apply to the target.
             Health = 0, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
             BackKickForce = 0, // Recoil.
