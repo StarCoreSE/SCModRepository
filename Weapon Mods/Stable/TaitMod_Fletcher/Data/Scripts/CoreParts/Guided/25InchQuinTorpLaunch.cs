80644d1eab4a36458014aa13fd0bc4a9994e0249
TaitKaito <clayton.h.c123@gmail.com>
2024-07-25 13:23:11 +0100

    Neptune launcher fix
    Reload to actual reload time, better heat management conntrol

--- Weapon Mods/Stable/TaitMod_Fletcher/Data/Scripts/CoreParts/Guided/25InchQuinTorpLaunch.cs
+++ Weapon Mods/Stable/TaitMod_Fletcher/Data/Scripts/CoreParts/Guided/25InchQuinTorpLaunch.cs
@@ -158,12 +158,12 @@ namespace Scripts {
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 36, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 3600, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 5, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 0, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 13, // Heat generated per shot.
-                    MaxHeat = 59, // Max heat before weapon enters cooldown (70% of max heat).
-                    Cooldown = .95f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
+                    MaxHeat = 60, // Max heat before weapon enters cooldown (70% of max heat).
+                    Cooldown = .1f, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
                     HeatSinkRate = 1, // Amount of heat lost per second.
                     DegradeRof = false, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 1, // Use this if you don't want the weapon to fire an entire physical magazine in one go. Should not be more than your magazine capacity.
