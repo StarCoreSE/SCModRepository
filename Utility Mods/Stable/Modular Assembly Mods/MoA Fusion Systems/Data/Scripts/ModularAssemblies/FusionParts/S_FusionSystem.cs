53898937b09fbe70abb988ef9fbf7eb57ffb2ce5
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-08-25 13:25:17 -0500

    Fusion Heat is now calculated by power generation rather than consumption

--- Utility Mods/Stable/Modular Assembly Mods/MoA Fusion Systems/Data/Scripts/ModularAssemblies/FusionParts/S_FusionSystem.cs
+++ Utility Mods/Stable/Modular Assembly Mods/MoA Fusion Systems/Data/Scripts/ModularAssemblies/FusionParts/S_FusionSystem.cs
@@ -201,6 +201,9 @@ namespace StarCore.FusionSystems.
                     thruster?.UpdatePower(powerGeneration, NewtonsPerFusionPower * generationModifier, Thrusters.Count);
             }
 
+            ModularApi.SetAssemblyProperty(PhysicalAssemblyId, "HeatGeneration",
+                PowerGeneration * MegawattsPerFusionPower);
+
             // Subtract power usage afterwards so that all reactors have the same stats.
             PowerGeneration = powerGeneration;
             MaxPowerStored = powerCapacity;
@@ -211,8 +214,6 @@ namespace StarCore.FusionSystems.
             PowerStored += PowerGeneration;
             if (PowerStored > MaxPowerStored) PowerStored = MaxPowerStored;
             //PowerGeneration = 0;
-            ModularApi.SetAssemblyProperty(PhysicalAssemblyId, "HeatGeneration",
-                PowerConsumption * MegawattsPerFusionPower);
         }
 
         public void UpdateTick()
