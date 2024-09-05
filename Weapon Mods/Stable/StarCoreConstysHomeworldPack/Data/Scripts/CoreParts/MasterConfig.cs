fb96adef52f5539d4f06beb580657cfd4492902d
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-09-05 02:22:36 -0500

    add CHP

---
+++ Weapon Mods/Stable/StarCoreConstysHomeworldPack/Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(CHP_IonTurret,
+							CHP_MediumIonTurret,
+							CHP_IonCannonFixed,
+							CHP_ArbiterKineticBurstCannon,
+							CHP_HeavyMassDriverTurret,
+							CHP_MediumKineticTurret,
+							CHP_MediumMassDriverSingleTurret,
+							CHP_MediumMassDriverDualTurret,
+							CHP_HeavyTorpedoLauncher,
+							CHP_SmallKineticTurretLG,
+							CHP_SmallKineticTurretSG,
+							CHP_SmallMassDriverTurretLG,
+							CHP_SmallMassDriverTurretSG,
+							CHP_KineticAutogun,
+							CHP_KineticAutogunPDC,
+							CHP_PlasmaBombLauncher,
+							CHP_FlakTurret,
+							
+							//drones
+							CHP_DroneHangarInterceptor_Hiigara,
+							CHP_DroneHangarBomber_Hiigara,
+							CHP_DroneHangarInterceptorQL_Hiigara,
+							CHP_DroneHangarBomberQL_Hiigara,
+							CHP_DroneHangarInterceptor_Hiigara_Rail,
+							CHP_DroneHangarBomber_Hiigara_Rail,
+							CHP_DroneHangarInterceptor_Vaygr,
+							CHP_DroneHangarBomber_Vaygr,
+							CHP_DroneHangarInterceptorQL_Vaygr,
+							CHP_DroneHangarBomberQL_Vaygr,
+							CHP_DroneHangarInterceptor_Vaygr_Rail,
+							CHP_DroneHangarBomber_Vaygr_Rail,
+							CHP_DroneHangarInterceptor_Taiidan,
+							CHP_DroneHangarBomber_Taiidan,
+							CHP_DroneHangarInterceptorQL_Taiidan,
+							CHP_DroneHangarBomberQL_Taiidan,
+							CHP_DroneHangarInterceptor_Taiidan_Rail,
+							CHP_DroneHangarBomber_Taiidan_Rail,
+							CHP_DroneHangarInterceptor_Kushan,
+							CHP_DroneHangarBomber_Kushan,
+							CHP_DroneHangarInterceptorQL_Kushan,
+							CHP_DroneHangarBomberQL_Kushan,
+							CHP_DroneHangarInterceptor_Kushan_Rail,
+							CHP_DroneHangarBomber_Kushan_Rail
+							);
+            
+        }
+    }
+}
