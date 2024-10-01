067e746e47bce60ebfee0335056ae2861d9cdecb
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-09-30 20:52:51 -0400

    cycle blocks is the devil
    also set apes base damage to 0

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/BonfireMissileLauncher.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/BonfireMissileLauncher.cs
@@ -63,9 +63,9 @@ namespace Scripts {
                 MaximumDiameter = 0, // Maximum radius of threat to engage; 0 = unlimited.
                 MaxTargetDistance = 5000, // Maximum distance at which targets will be automatically shot at; 0 = unlimited.
                 MinTargetDistance = 0, // Minimum distance at which targets will be automatically shot at.
-                TopTargets = 10, // Maximum number of targets to randomize between; 0 = unlimited.
+                TopTargets = 6, // Maximum number of targets to randomize between; 0 = unlimited.
                 CycleTargets = 0, // Number of targets to "cycle" per acquire attempt.
-                TopBlocks = 10, // Maximum number of blocks to randomize between; 0 = unlimited.
+                TopBlocks = 18, // Maximum number of blocks to randomize between; 0 = unlimited.
                 CycleBlocks = 0, // Number of blocks to "cycle" per acquire attempt.
                 StopTrackingSpeed = 0, // Do not track threats traveling faster than this speed; 0 = unlimited.
             },
