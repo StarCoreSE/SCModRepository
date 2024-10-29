b1ef077a366e2d8794bd394d093f9c20fdb1314a
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-10-28 19:56:38 -0500

    API

---
+++ Utility Mods/Stable/GyroscopicGunsight/Data/Scripts/GyroscopicGunsight/GyroscopicGunsightSession.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SC.GyroscopicGunsight.API.CoreSystems;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Utils;
+using VRageMath;
+
+namespace SC.GyroscopicGunsight
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    internal class GyroscopicGunsightSession : MySessionComponentBase
+    {
+        public WcApi WcApi = new WcApi();
+
+        public override void LoadData()
+        {
+            WcApi.Load();
+        }
+
+        public override void Draw()
+        {
+            if (!(WcApi?.IsReady ?? false))
+                return;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MyLog.Default.WriteLine(ex);
+            }
+        }
+
+        public Vector3D CalculateDeflection(IMyCubeBlock thisWeapon, IMyCubeGrid targetGrid)
+        {
+            // TODO
+            return Vector3D.Zero;
+        }
+    }
+}
