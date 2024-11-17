dd56ea2ac56358e1ea877bd7a98594e434f9df8a
FP-Anomaly <carbonjvd@gmail.com>
2024-11-17 07:34:17 -0800

    Hercules Ammo Rework - now with more shotgun (#1799)
    * SOL Tweaks
    
    Nariman:
    - Projectile HP 4 -> 80
    - Projectile speed 1500 -> 1700
    Goncol:
    - Shield modifier 2000 -> 1200
    - Bypass -1 -> 0.25
    
    * Update Nariman_Turret_Ammo.cs
    
    Projectile Health: 80 -> 40
    
    * SOL Tweakage
    
    Hercules MAC:
    - Projectile Speed: 1800 ->2200
    
    Nariman NanoDart Turrt:
    - Particles less bright (I hope)
    
    SM-3 Counter Battery:
    - Aim Tolerance: 180 -> 270
    - Add Tolerance to Tracking: false -> true
    
    * Update Heavy_MAC_Ammo.cs
    
    Projectile Health nullified
    
    * Herc Ammo Rework
    
    Now fires smart (unguided) projectiles that explode into a targeted shotgun blast. If locked on, will shotgun within 1km. If not locked in and overridden, will act like the classic triple fire Hercules.
    
    Also did some cubeblock clean up.

--- Weapon Mods/Stable/Anomaly_Solaris_Hypernautics/Data/Scripts/CoreParts/Heavy_MAC.cs
+++ Weapon Mods/Stable/Anomaly_Solaris_Hypernautics/Data/Scripts/CoreParts/Heavy_MAC.cs
@@ -55,8 +55,8 @@ namespace Scripts {
             HardPoint = new HardPointDef
             {
                 PartName = "Hercules MAC", // Name of the weapon in terminal, should be unique for each weapon definition that shares a SubtypeId (i.e. multiweapons).
-                DeviateShotAngle = 0.1f, // Projectile inaccuracy in degrees.
-                AimingTolerance = 0f, // How many degrees off target a turret can fire at. 0 - 180 firing angle.
+                DeviateShotAngle = 0f, // Projectile inaccuracy in degrees.
+                AimingTolerance = 3f, // How many degrees off target a turret can fire at. 0 - 180 firing angle.
                 AimLeadingPrediction = Accurate, // Level of turret aim prediction; Off, Basic, Accurate, Advanced
                 DelayCeaseFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 second, etc..). Length of time the weapon continues firing after trigger is released - while a target is available.
                 AddToleranceToTracking = false, // Allows turret to track to the edge of the AimingTolerance cone instead of dead centre.
