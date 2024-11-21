f65afd8907f749dabb6665773abc589bf02e3831
FP-Anomaly <carbonjvd@gmail.com>
2024-11-20 20:12:39 -0800

    Frag and Slug Damage nerfs (#1807)
    * SOL Tweaks
    
    Nariman:
    - Projectile HP 4 -> 80
    - Projectile speed 1500 -> 1700
    Goncol:
    - Shield modifier 2000 -> 1200
    - Bypass -1 -> 0.25
    
    * Update Nariman_Turret_Ammo.cs
    
    Projectile Health: 80 -> 40
    
    * SOL Tweakage
    
    Hercules MAC:
    - Projectile Speed: 1800 ->2200
    
    Nariman NanoDart Turrt:
    - Particles less bright (I hope)
    
    SM-3 Counter Battery:
    - Aim Tolerance: 180 -> 270
    - Add Tolerance to Tracking: false -> true
    
    * Update Heavy_MAC_Ammo.cs
    
    Projectile Health nullified
    
    * Herc Ammo Rework
    
    Now fires smart (unguided) projectiles that explode into a targeted shotgun blast. If locked on, will shotgun within 1km. If not locked in and overridden, will act like the classic triple fire Hercules.
    
    Also did some cubeblock clean up.
    
    * Update Heavy_MAC_Ammo.cs
    
    Shrap Tap
    Frag Damage: 33750 -> 10050
    Firing Cone Degrees: 6 -> 4
    
    * Update Heavy_MAC_Ammo.cs
    
    More Nerfage
    Slower Slug: 3400 -> 2200
    Slower Frag: 2800 -> 1600
    Frag Distance: 6750 -> 1150
    
    * Update Heavy_MAC_Ammo.cs
    
    Damage Nerfs

--- Weapon Mods/Stable/Anomaly_Solaris_Hypernautics/Data/Scripts/CoreParts/Heavy_MAC_Ammo.cs
+++ Weapon Mods/Stable/Anomaly_Solaris_Hypernautics/Data/Scripts/CoreParts/Heavy_MAC_Ammo.cs
@@ -40,7 +40,7 @@ namespace Scripts
             AmmoRound = "Heavy MAC Slug", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
             HybridRound = true, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 0.0645f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
-            BaseDamage = 75000f, // Direct damage; one steel plate is worth 100.
+            BaseDamage = 10500f, // Direct damage; one steel plate is worth 100.
             Mass = 10f, // In kilograms; how much force the impact will apply to the target.
             Health = 200, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
             BackKickForce = 500000f, // Recoil. This is applied to the Parent Grid.
@@ -124,8 +124,8 @@ namespace Scripts
                 {
                     Armor = -1f, // Multiplier for damage against all armor. This is multiplied with the specific armor type multiplier (light, heavy).
                     Light = -1f, // Multiplier for damage against light armor.
-                    Heavy = -1f, // Multiplier for damage against heavy armor.
-                    NonArmor = 0.75f, // Multiplier for damage against every else.
+                    Heavy = 0.5f, // Multiplier for damage against heavy armor.
+                    NonArmor = 0.25f, // Multiplier for damage against every else.
                 },
                 Shields = new ShieldDef
                 {
@@ -428,7 +428,7 @@ namespace Scripts
             AmmoRound = "SolHyp_HeavyMAC_Shrap", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
             HybridRound = false, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 0.1f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
-            BaseDamage = 10050f, // Direct damage; one steel plate is worth 100.
+            BaseDamage = 5050f, // Direct damage; one steel plate is worth 100.
             Mass = 10f, // In kilograms; how much force the impact will apply to the target.
             Health = 0, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
             BackKickForce = 0f, // Recoil. This is applied to the Parent Grid.
@@ -512,8 +512,8 @@ namespace Scripts
                 {
                     Armor = -1f, // Multiplier for damage against all armor. This is multiplied with the specific armor type multiplier (light, heavy).
                     Light = -1f, // Multiplier for damage against light armor.
-                    Heavy = -1f, // Multiplier for damage against heavy armor.
-                    NonArmor = -1f, // Multiplier for damage against every else.
+                    Heavy = 0.5f, // Multiplier for damage against heavy armor.
+                    NonArmor = 0.25f, // Multiplier for damage against every else.
                 },
                 Shields = new ShieldDef
                 {
