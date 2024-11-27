3e0a2e2a6607bb7cc4cfe4604e8e43ebfcdb4300
Aristeas <94058548+ari-steas@users.noreply.github.com>
2024-11-26 22:43:20 -0600

    Automatic faction setting for /sc clearboard (#1825)

--- Gamemode Mods/Stable/StarCore SUGMA Gamemodes/Data/Scripts/SUGMA/HeartNetworking/Custom/ClearBoardRequestPacket.cs
+++ Gamemode Mods/Stable/StarCore SUGMA Gamemodes/Data/Scripts/SUGMA/HeartNetworking/Custom/ClearBoardRequestPacket.cs
@@ -7,9 +7,20 @@ namespace SC.SUGMA.HeartNetworking.Custom
     [ProtoContract]
     internal class ClearBoardRequestPacket : PacketBase
     {
+        [ProtoMember(1)] public bool ResetFactions;
+
+        public ClearBoardRequestPacket(bool resetFactions)
+        {
+            ResetFactions = resetFactions;
+        }
+
+        private ClearBoardRequestPacket()
+        {
+        }
+
         public override void Received(ulong SenderSteamId)
         {
-            SUtils.ClearBoard();
+            SUtils.ClearBoard(ResetFactions);
             if (MyAPIGateway.Session.IsServer)
                 HeartNetwork.I.SendToEveryone(this);
         }
