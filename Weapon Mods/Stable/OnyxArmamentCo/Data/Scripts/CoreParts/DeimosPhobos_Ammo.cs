054dedd01dd0985f054015e321fed61f99c1a014
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2024-11-28 18:43:27 -0600

    This Just In: This (#1829)

--- Weapon Mods/Stable/OnyxArmamentCo/Data/Scripts/CoreParts/DeimosPhobos_Ammo.cs
+++ Weapon Mods/Stable/OnyxArmamentCo/Data/Scripts/CoreParts/DeimosPhobos_Ammo.cs
@@ -119,8 +119,8 @@ namespace Scripts
                 {
                     Armor = -1f, // Multiplier for damage against all armor. This is multiplied with the specific armor type multiplier (light, heavy).
                     Light = -1f, // Multiplier for damage against light armor.
-                    Heavy = -1f, // Multiplier for damage against heavy armor.
-                    NonArmor = -1f, // Multiplier for damage against every else.
+                    Heavy = 0.45f, // Multiplier for damage against heavy armor.
+                    NonArmor = 0.8f, // Multiplier for damage against every else.
                 },
                 Shields = new ShieldDef
                 {
