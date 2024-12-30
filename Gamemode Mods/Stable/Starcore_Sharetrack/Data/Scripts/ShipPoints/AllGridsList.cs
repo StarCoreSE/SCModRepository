3f6b313c193c474694c0820f9df62864f7c938a2
CommodoreChet <50426713+CommodoreChet@users.noreply.github.com>
2024-12-30 13:04:25 -0600

    Revert "Impliment ShareTrack Siege Indicator"
    This reverts commit 7e14b30f9e850f713c16cf5afeeaf1099bc14fe4.

--- Gamemode Mods/Stable/Starcore_Sharetrack/Data/Scripts/ShipPoints/AllGridsList.cs
+++ Gamemode Mods/Stable/Starcore_Sharetrack/Data/Scripts/ShipPoints/AllGridsList.cs
@@ -264,18 +264,8 @@ namespace StarCore.ShareTrack
             var functionalColor = tracker.IsFunctional ? "white" : "red";
             var integrityColor = integrityPercent >= 75 ? "White" : integrityPercent >= 50 ? "LightCoral" : integrityPercent >= 25 ? "IndianRed" : "FireBrick";
 
-            var siegeColor = "white";
-            string siegeDisplay = "";
-            if (tracker.FieldGenerator != null)
-            {
-                siegeDisplay = tracker.IsSiegeActive ? "[SIEGED] " : "";
-                siegeColor = tracker.IsSiegeActive ? tracker.GeneratorDisplayBlink == 1 ? "red" : "yellow" : "white";
-
-                tracker.GeneratorDisplayBlink = 1 - tracker.GeneratorDisplayBlink;
-            }
-
             return
-                $"<color={siegeColor}>{siegeDisplay}<color={functionalColor}>{ownerDisplay,-8}<color={integrityColor}>{integrityPercent,3}%<color={functionalColor}> P:<color=orange>{power,3}<color={functionalColor}> T:<color=orange>{thrust,3}<color={functionalColor}> W:<color={weaponColor}>{wep}<color={functionalColor}> S:<color={shieldColor}>{shieldPercent,3}%<color=white>";
+                $"<color={functionalColor}>{ownerDisplay,-8}<color={integrityColor}>{integrityPercent,3}%<color={functionalColor}> P:<color=orange>{power,3}<color={functionalColor}> T:<color=orange>{thrust,3}<color={functionalColor}> W:<color={weaponColor}>{wep}<color={functionalColor}> S:<color={shieldColor}>{shieldPercent,3}%<color=white>";
         }
 
 
@@ -332,7 +322,6 @@ namespace StarCore.ShareTrack
         public WcApi WcApi { get; private set; }
         public ShieldApi ShieldApi { get; private set; }
         public RtsApi RtsApi { get; private set; }
-        public FieldGeneratorAPI FieldGeneratorAPI { get; private set; }
 
         private HudPointsList _hudPointsList;
 
@@ -383,9 +372,6 @@ namespace StarCore.ShareTrack
             // Initialize the RTS_api and load it if it's not null
             RtsApi = new RtsApi();
             RtsApi?.Load();
-
-            FieldGeneratorAPI = new FieldGeneratorAPI();
-            FieldGeneratorAPI?.LoadAPI();
         }
 
         public void Close()
@@ -395,7 +381,6 @@ namespace StarCore.ShareTrack
             WcApi?.Unload();
             ShieldApi?.Unload();
             RtsApi?.Unload();
-            FieldGeneratorAPI?.UnloadAPI();
             if (PointValues != null)
             {
                 PointValues.Clear();
--- Gamemode Mods/Stable/Starcore_Sharetrack/Data/Scripts/ShipPoints/ShipTracking/ShipTracker.cs
+++ Gamemode Mods/Stable/Starcore_Sharetrack/Data/Scripts/ShipPoints/ShipTracking/ShipTracker.cs
@@ -124,7 +124,6 @@ namespace StarCore.ShareTrack.ShipTracking
         }
 
         private ShieldApi ShieldApi => AllGridsList.I.ShieldApi;
-        private FieldGeneratorAPI FieldGeneratorAPI => AllGridsList.I.FieldGeneratorAPI;
 
 
         public IMyCubeGrid Grid { get; private set; }
@@ -656,33 +655,6 @@ namespace StarCore.ShareTrack.ShipTracking
 
         #endregion
 
-        #region Field Generator Stats
-        public IMyFunctionalBlock FieldGenerator => FieldGeneratorAPI.GetFirstFieldGeneratorOnGrid(Grid.EntityId);
-
-        public bool IsSiegeActive
-        {
-            get
-            {
-                var fieldGenerator = FieldGeneratorAPI.GetFirstFieldGeneratorOnGrid(Grid.EntityId);
-                if (fieldGenerator == null) 
-                    return false;
-                return FieldGeneratorAPI.IsSiegeActive(fieldGenerator);
-            }
-        }
-
-        public float CurrentFieldPower
-        {
-            get
-            {
-                var fieldGenerator = FieldGeneratorAPI.GetFirstFieldGeneratorOnGrid(Grid.EntityId);
-                if (fieldGenerator == null)
-                    return 0;
-                return FieldGeneratorAPI.GetFieldPower(fieldGenerator);
-            }
-        }
-
-        public int GeneratorDisplayBlink;
-        #endregion
         #endregion
     }
 }
