6c56824eff11519a01c5e6758767495fcdca55c1
Aristeas <94058548+ari-steas@users.noreply.github.com>
2025-03-06 00:31:09 -0600

    aa

--- Gamemode Mods/Starcore_Sharetrack/Data/Scripts/ShipPoints/ShipTracking/ShipTracker.cs
+++ Gamemode Mods/Starcore_Sharetrack/Data/Scripts/ShipPoints/ShipTracking/ShipTracker.cs
@@ -351,6 +351,14 @@ namespace StarCore.ShareTrack.ShipTracking
                     camera.WorldMatrix.Forward);
 
                 var stealthed = ((uint)Grid.Flags & 0x1000000) > 0;
+
+                if (MasterSession.I.StealthApi?.IsReady ?? false)
+                {
+                    var mainStealthDrive = MasterSession.I.StealthApi.GetMainDrive(Grid);
+                    if (mainStealthDrive != null)
+                        stealthed |= MasterSession.I.StealthApi.GetStatus(mainStealthDrive) == 1;
+                }
+
                 var visible = !(newOrigin.X > 1 || newOrigin.X < -1 || newOrigin.Y > 1 || newOrigin.Y < -1) &&
                               angle <= fov && !stealthed;
 
