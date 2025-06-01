bace2b64a1fe535d5f22ae0bdcf78c720fedadfd
Th3-spook <165436671+Th3-spook@users.noreply.github.com>
2025-06-01 09:20:28 -0700

    Revert "Season 5"

--- Weapon Mods/TIOStarcore/Data/Scripts/CoreParts/Plasma Weapons/X-4PlasAmmo.cs
+++ Weapon Mods/TIOStarcore/Data/Scripts/CoreParts/Plasma Weapons/X-4PlasAmmo.cs
@@ -42,7 +42,7 @@ namespace Scripts
             AmmoMagazine = "PlasAmmo", // SubtypeId of physical ammo magazine. Use "Energy" for weapons without physical ammo.
             AmmoRound = "Plasma-X", // Name of ammo in terminal, should be different for each ammo type used by the same weapon.
             HybridRound = true, // Use both a physical ammo magazine and energy per shot.
-            EnergyCost = 0.4f, //120MW@0.823  //58MW@0.4  // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
+            EnergyCost = 0.823f, //120MW@0.823  //58MW@0.4  // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
             BaseDamage = 5000f, // Direct damage; one steel plate is worth 100. 
             Mass = 100f, // In kilograms; how much force the impact will apply to the target.
             Health = 0, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
