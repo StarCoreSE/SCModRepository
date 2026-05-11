637527de298e9242aed187fc3e2dc9cc2da8700b
KingParmenides <parmenides0000@gmail.com>
2026-05-11 05:34:10 -0600

    Add grid heat API

---
+++ Utility Mods/MoA Fusion Systems/Data/Scripts/ModularAssemblies/HeatParts/GridHeatApi.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.ModAPI;
+using VRage.Game.ModAPI;
+
+namespace Epstein_Fusion_DS.HeatParts
+{
+    /// <summary>
+    /// Copy this class into another mod and call Load() in LoadData() to use the grid heat API.
+    /// </summary>
+    public sealed class GridHeatApi
+    {
+        private const long Channel = 11920040L;
+        private const string ApiRequest = "GridHeatAPI.Request";
+
+        private Action _onReady;
+        private Dictionary<string, Delegate> _api;
+        private bool _registered;
+
+        public bool IsReady => _api != null;
+
+        public void Load(Action onReady = null)
+        {
+            if (_registered)
+                return;
+
+            _onReady = onReady;
+            _registered = true;
+            MyAPIGateway.Utilities.RegisterMessageHandler(Channel, HandleMessage);
+            MyAPIGateway.Utilities.SendModMessage(Channel, ApiRequest);
+        }
+
+        public void Unload()
+        {
+            if (!_registered)
+                return;
+
+            MyAPIGateway.Utilities.UnregisterMessageHandler(Channel, HandleMessage);
+            _registered = false;
+            _api = null;
+            _onReady = null;
+        }
+
+        public int Version()
+        {
+            var method = Get<Func<int>>("Version");
+            return method?.Invoke() ?? 0;
+        }
+
+        public bool AddHeat(IMyCubeGrid grid, float heat)
+        {
+            var method = Get<Func<IMyCubeGrid, float, bool>>("AddHeat");
+            return method != null && method(grid, heat);
+        }
+
+        public bool SetHeat(IMyCubeGrid grid, float heat)
+        {
+            var method = Get<Func<IMyCubeGrid, float, bool>>("SetHeat");
+            return method != null && method(grid, heat);
+        }
+
+        public float GetHeat(IMyCubeGrid grid)
+        {
+            var method = Get<Func<IMyCubeGrid, float>>("GetHeat");
+            return method?.Invoke(grid) ?? -1;
+        }
+
+        public float GetHeatRatio(IMyCubeGrid grid)
+        {
+            var method = Get<Func<IMyCubeGrid, float>>("GetHeatRatio");
+            return method?.Invoke(grid) ?? -1;
+        }
+
+        public float GetHeatCapacity(IMyCubeGrid grid)
+        {
+            var method = Get<Func<IMyCubeGrid, float>>("GetHeatCapacity");
+            return method?.Invoke(grid) ?? -1;
+        }
+
+        public float GetHeatDissipation(IMyCubeGrid grid)
+        {
+            var method = Get<Func<IMyCubeGrid, float>>("GetHeatDissipation");
+            return method?.Invoke(grid) ?? -1;
+        }
+
+        public float GetHeatGeneration(IMyCubeGrid grid)
+        {
+            var method = Get<Func<IMyCubeGrid, float>>("GetHeatGeneration");
+            return method?.Invoke(grid) ?? -1;
+        }
+
+        public bool SetHeatCapacity(IMyCubeGrid grid, float capacity)
+        {
+            var method = Get<Func<IMyCubeGrid, float, bool>>("SetHeatCapacity");
+            return method != null && method(grid, capacity);
+        }
+
+        public bool SetHeatDissipation(IMyCubeGrid grid, float dissipation)
+        {
+            var method = Get<Func<IMyCubeGrid, float, bool>>("SetHeatDissipation");
+            return method != null && method(grid, dissipation);
+        }
+
+        public bool SetHeatGeneration(IMyCubeGrid grid, float generation)
+        {
+            var method = Get<Func<IMyCubeGrid, float, bool>>("SetHeatGeneration");
+            return method != null && method(grid, generation);
+        }
+
+        public void SetHudVisible(bool visible)
+        {
+            Get<Action<bool>>("SetHudVisible")?.Invoke(visible);
+        }
+
+        public void SetHudOffset(float x, float y, float z)
+        {
+            Get<Action<float, float, float>>("SetHudOffset")?.Invoke(x, y, z);
+        }
+
+        private T Get<T>(string name) where T : class
+        {
+            if (_api == null || !_api.ContainsKey(name))
+                return null;
+
+            return _api[name] as T;
+        }
+
+        private void HandleMessage(object obj)
+        {
+            var api = obj as Dictionary<string, Delegate>;
+            if (api == null)
+                return;
+
+            _api = api;
+            _onReady?.Invoke();
+            _onReady = null;
+        }
+    }
+}
--- Utility Mods/MoA Fusion Systems/Data/Scripts/ModularAssemblies/HeatParts/HeatManager.cs
+++ Utility Mods/MoA Fusion Systems/Data/Scripts/ModularAssemblies/HeatParts/HeatManager.cs
@@ -1,31 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Sandbox.ModAPI;
 using VRage.Game.ModAPI;
 using VRage.ModAPI;
+using VRageMath;
 
 namespace Epstein_Fusion_DS.HeatParts
 {
     internal class HeatManager
     {
+        public const long ApiChannel = 11920040L;
+        private const string ApiRequest = "GridHeatAPI.Request";
+
         public static HeatManager I = new HeatManager();
 
         private readonly Dictionary<IMyCubeGrid, GridHeatManager> _heatSystems =
             new Dictionary<IMyCubeGrid, GridHeatManager>();
+
+        private Dictionary<string, Delegate> _api;
+
+        public bool HeatHudVisible = true;
+        public Vector3D HeatHudOffset = new Vector3D(-0.759375, -0.8, 0);
 
         public void Load()
         {
             I = this;
             MyAPIGateway.Entities.OnEntityAdd += OnEntityAdd;
             MyAPIGateway.Entities.OnEntityRemove += OnEntityRemove;
+            MyAPIGateway.Utilities.RegisterMessageHandler(ApiChannel, HandleApiRequest);
+            RegisterExistingGrids();
         }
 
         public void Unload()
         {
             MyAPIGateway.Entities.OnEntityAdd -= OnEntityAdd;
             MyAPIGateway.Entities.OnEntityRemove -= OnEntityRemove;
+            MyAPIGateway.Utilities.UnregisterMessageHandler(ApiChannel, HandleApiRequest);
             foreach (var system in _heatSystems.Values)
                 system.Unload();
+            _heatSystems.Clear();
+            _api = null;
             I = null;
         }
 
@@ -46,9 +61,14 @@ namespace Epstein_Fusion_DS.HeatParts
             return _heatSystems.GetValueOrDefault(grid, null)?.HeatRatio ?? -1;
         }
 
+        public float GetGridHeat(IMyCubeGrid grid)
+        {
+            return _heatSystems.GetValueOrDefault(grid, null)?.HeatStored ?? -1;
+        }
+
         public float GetGridHeatCapacity(IMyCubeGrid grid)
         {
-            return _heatSystems.GetValueOrDefault(grid, null)?.HeatCapacity ?? -1;
+            return _heatSystems.GetValueOrDefault(grid, null)?.TotalHeatCapacity ?? -1;
         }
 
         public float GetGridHeatDissipation(IMyCubeGrid grid)
@@ -61,13 +81,117 @@ namespace Epstein_Fusion_DS.HeatParts
             return _heatSystems.GetValueOrDefault(grid, null)?.HeatGeneration ?? -1;
         }
 
+        public bool HasGridHeat(IMyCubeGrid grid)
+        {
+            return _heatSystems.GetValueOrDefault(grid, null)?.HasApiHeat ?? false;
+        }
+
+        public bool AddGridHeat(IMyCubeGrid grid, float heat)
+        {
+            var system = EnsureGrid(grid);
+            if (system == null)
+                return false;
+
+            system.AddHeat(heat);
+            return true;
+        }
+
+        public bool SetGridHeat(IMyCubeGrid grid, float heat)
+        {
+            var system = EnsureGrid(grid);
+            if (system == null)
+                return false;
+
+            system.SetHeat(heat);
+            return true;
+        }
+
+        public bool SetGridHeatCapacity(IMyCubeGrid grid, float capacity)
+        {
+            var system = EnsureGrid(grid);
+            if (system == null)
+                return false;
+
+            system.ApiHeatCapacity = Math.Max(0, capacity);
+            system.SetHeat(system.HeatStored);
+            return true;
+        }
+
+        public bool SetGridHeatDissipation(IMyCubeGrid grid, float dissipation)
+        {
+            var system = EnsureGrid(grid);
+            if (system == null)
+                return false;
+
+            system.ApiHeatDissipation = Math.Max(0, dissipation);
+            return true;
+        }
+
+        public bool SetGridHeatGeneration(IMyCubeGrid grid, float generation)
+        {
+            var system = EnsureGrid(grid);
+            if (system == null)
+                return false;
+
+            system.ApiHeatGeneration = generation;
+            system.HeatGeneration = generation;
+            return true;
+        }
+
+        public void SetHeatHudVisible(bool visible)
+        {
+            HeatHudVisible = visible;
+        }
+
+        public void SetHeatHudOffset(float x, float y, float z)
+        {
+            HeatHudOffset = new Vector3D(x, y, z);
+        }
+
+        private void HandleApiRequest(object obj)
+        {
+            if (!(obj is string) || (string)obj != ApiRequest)
+                return;
+
+            MyAPIGateway.Utilities.SendModMessage(ApiChannel, GetApi());
+        }
+
+        private Dictionary<string, Delegate> GetApi()
+        {
+            return _api ?? (_api = new Dictionary<string, Delegate>
+            {
+                ["Version"] = new Func<int>(() => 1),
+                ["AddHeat"] = new Func<IMyCubeGrid, float, bool>(AddGridHeat),
+                ["SetHeat"] = new Func<IMyCubeGrid, float, bool>(SetGridHeat),
+                ["GetHeat"] = new Func<IMyCubeGrid, float>(GetGridHeat),
+                ["GetHeatRatio"] = new Func<IMyCubeGrid, float>(GetGridHeatLevel),
+                ["GetHeatCapacity"] = new Func<IMyCubeGrid, float>(GetGridHeatCapacity),
+                ["GetHeatDissipation"] = new Func<IMyCubeGrid, float>(GetGridHeatDissipation),
+                ["GetHeatGeneration"] = new Func<IMyCubeGrid, float>(GetGridHeatGeneration),
+                ["SetHeatCapacity"] = new Func<IMyCubeGrid, float, bool>(SetGridHeatCapacity),
+                ["SetHeatDissipation"] = new Func<IMyCubeGrid, float, bool>(SetGridHeatDissipation),
+                ["SetHeatGeneration"] = new Func<IMyCubeGrid, float, bool>(SetGridHeatGeneration),
+                ["SetHudVisible"] = new Action<bool>(SetHeatHudVisible),
+                ["SetHudOffset"] = new Action<float, float, float>(SetHeatHudOffset),
+            });
+        }
+
+        private void RegisterExistingGrids()
+        {
+            var entities = new HashSet<IMyEntity>();
+            MyAPIGateway.Entities.GetEntities(entities, entity => entity is IMyCubeGrid && entity.Physics != null);
+
+            foreach (var entity in entities)
+                EnsureGrid((IMyCubeGrid)entity);
+        }
+
         private void OnEntityAdd(IMyEntity entity)
         {
             if (!(entity is IMyCubeGrid) || entity.Physics == null)
                 return;
             var grid = (IMyCubeGrid)entity;
 
-            _heatSystems[grid] = new GridHeatManager(grid);
+            EnsureGrid(grid);
         }
 
         private void OnEntityRemove(IMyEntity entity)
@@ -76,18 +200,32 @@ namespace Epstein_Fusion_DS.HeatParts
                 return;
             var grid = (IMyCubeGrid)entity;
 
+            if (!_heatSystems.ContainsKey(grid))
+                return;
+
             _heatSystems[grid].Unload();
             _heatSystems.Remove(grid);
         }
 
         public void OnPartAdd(int assemblyId, IMyCubeBlock block, bool isBaseBlock)
         {
-            _heatSystems[block.CubeGrid].OnPartAdd(assemblyId, block, isBaseBlock);
+            EnsureGrid(block.CubeGrid)?.OnPartAdd(assemblyId, block, isBaseBlock);
         }
 
         public void OnPartRemove(int assemblyId, IMyCubeBlock block, bool isBaseBlock)
         {
-            _heatSystems[block.CubeGrid].OnPartRemove(assemblyId, block, isBaseBlock);
+            EnsureGrid(block.CubeGrid)?.OnPartRemove(assemblyId, block, isBaseBlock);
+        }
+
+        private GridHeatManager EnsureGrid(IMyCubeGrid grid)
+        {
+            if (grid == null)
+                return null;
+
+            if (!_heatSystems.ContainsKey(grid))
+                _heatSystems[grid] = new GridHeatManager(grid);
+
+            return _heatSystems[grid];
         }
     }
 }
